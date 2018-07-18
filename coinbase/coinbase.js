787dc01e8f59ad4f62574e6bb79ac6fa14eba58e
karabaja4 <karabaja4@gmail.com>
2018-07-18 04:42:14 +0200

    a

--- coinbase/coinbase.js
+++ coinbase/coinbase.js
@@ -2,14 +2,19 @@ var coinbase = require('coinbase');
 var secret = require('./secret.json')
 var client   = new coinbase.Client({'apiKey': secret.apiKey, 'apiSecret': secret.apiSecret});
 
+const print = (text) =>
+{
+	console.log('<span foreground="aqua">' + text + '</span>');
+}
+
 const get = () =>
 {
     client.getAccount('a954c724-975b-54d1-824c-aa2e6dffe766', (err, account) =>
     {
-        if (account != null)
-        {
-            console.log('<span foreground="aqua">' + account.native_balance.amount + ' ' + account.native_balance.currency + '</span>');
-        }
+		if (!err && account != null)
+		{
+			print(account.native_balance.amount + ' ' + account.native_balance.currency);
+		}
     });
 };
 
