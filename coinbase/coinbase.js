9cedd06f8c5d2db9c674e29b5513530ddd48f52c
karabaja4 <karabaja4@gmail.com>
2018-08-29 00:19:26 +0200

    a

--- coinbase/coinbase.js
+++ coinbase/coinbase.js
@@ -1,25 +1,36 @@
-var coinbase = require('coinbase');
-var secret = require('./secret.json')
-var client   = new coinbase.Client({'apiKey': secret.apiKey, 'apiSecret': secret.apiSecret});
+const coinbase = require('coinbase');
+const secret = require('./secret.json')
+const client   = new coinbase.Client({'apiKey': secret.apiKey, 'apiSecret': secret.apiSecret});
+const { exec } = require('child_process');
+const async = require('async');
 
-const print = (text) =>
-{
-	console.log('<span foreground="aqua">' + text + '</span>');
+const print = (text1, text2) => {
+	console.log(`<span foreground="aqua">${text1}</span><span foreground="white">  |  </span><span foreground="lime">${text2}</span>`);
 }
 
-const get = () =>
-{
-    client.getAccount('a954c724-975b-54d1-824c-aa2e6dffe766', (err, account) =>
-    {
-		if (!err && account != null)
-		{
-			print(account.native_balance.amount + ' ' + account.native_balance.currency);
+const coinbaseGet = (callback) => {
+    client.getAccount('a954c724-975b-54d1-824c-aa2e6dffe766', (err, account) => {
+		try {
+			callback(null, !err && account != null ? (account.native_balance.amount + " " + account.native_balance.currency) : "ERROR");
+		} catch (e) {
+			callback(null, "ERROR");
 		}
     });
 };
 
-get();
-//setInterval(() => {
-//    get();
-//}, 30000);
+const pbzGet = (callback) => {
+	exec("curl -s -f " + secret.pbzCurl.slice(5), (error, stdout, stderr) => {
+		try {
+			const data = JSON.parse(stdout);
+			const balance = data.jsonResponse.result.homePageCommandList[0].data.tableData[0].bankAccounts[0].balance.amount;
+			const currency = data.jsonResponse.result.homePageCommandList[0].data.tableData[0].bankAccounts[0].accountCurrencyCode;
+			callback(null, balance + " " + currency);
+		} catch (e) {
+			callback(null, "ERROR");
+		}
+	});
+}
 
+async.parallel([pbzGet, coinbaseGet], (err, results) => {
+	print(results[0], results[1]);
+});
