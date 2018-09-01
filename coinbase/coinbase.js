422f01b69ffa6ba27d1d3b47bee9056382bfff3d
karabaja4 <karabaja4@gmail.com>
2018-09-01 15:59:01 +0200

    a

--- coinbase/coinbase.js
+++ coinbase/coinbase.js
@@ -4,33 +4,40 @@ const client   = new coinbase.Client({'apiKey': secret.apiKey, 'apiSecret': secr
 const { exec } = require('child_process');
 const async = require('async');
 
+let pbz = true;
+
 const print = (text1, text2) => {
-	console.log(`<span foreground="aqua">${text1}</span><span foreground="white">  |  </span><span foreground="lime">${text2}</span>`);
+	console.log(`<span foreground="aqua">${text1 || "-"}</span><span foreground="white">  |  </span><span foreground="lime">${text2 || "-"}</span>`);
 }
 
 const coinbaseGet = (callback) => {
     client.getAccount('a954c724-975b-54d1-824c-aa2e6dffe766', (err, account) => {
 		try {
-			callback(null, !err && account != null ? (account.native_balance.amount + " " + account.native_balance.currency) : "ERROR");
+			callback(null, !err && account != null ? (account.native_balance.amount + " " + account.native_balance.currency) : null);
 		} catch (e) {
-			callback(null, "-");
+			callback(null, null);
 		}
     });
 };
 
 const pbzGet = (callback) => {
-	exec("curl -s -f " + secret.pbzCurl.slice(5), (error, stdout, stderr) => {
-		try {
-			const data = JSON.parse(stdout);
-			const balance = data.jsonResponse.result.homePageCommandList[0].data.tableData[0].bankAccounts[0].balance.amount;
-			const currency = data.jsonResponse.result.homePageCommandList[0].data.tableData[0].bankAccounts[0].accountCurrencyCode;
-			callback(null, balance + " " + currency);
-		} catch (e) {
-			callback(null, "-");
-		}
-	});
+	if (pbz) {
+		exec("curl -s -f " + secret.pbzCurl.slice(5), (error, stdout, stderr) => {
+			try {
+				const data = JSON.parse(stdout);
+				const balance = data.jsonResponse.result.homePageCommandList[0].data.tableData[0].bankAccounts[0].balance.amount;
+				const currency = data.jsonResponse.result.homePageCommandList[0].data.tableData[0].bankAccounts[0].accountCurrencyCode;
+				callback(null, balance + " " + currency);
+			} catch (e) {
+				callback(null, null);
+				pbz = false;
+			}
+		});
+	} else {
+		callback(null, null);
+	}
 }
 
-async.parallel([pbzGet, coinbaseGet], (err, results) => {
+async.parallel([coinbaseGet, pbzGet], (err, results) => {
 	print(results[0], results[1]);
 });
