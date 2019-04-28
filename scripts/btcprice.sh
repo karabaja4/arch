d56edab8e57273327cf6d6889d56deb5189b1969
karabaja4 <karabaja4@gmail.com>
2019-04-28 04:56:44 +0200

    a

--- scripts/btcprice.sh
+++ scripts/btcprice.sh
@@ -16,33 +16,36 @@ rm -f $conkyfile
 while true
 do
 	result=$(curl -s https://api.coinbase.com/v2/prices/BTC-USD/spot)
-	if [ $? == 0 ]; then
+	if [[ $? == 0 ]]; then
 
 		price=$(echo $result | jq -r '.data .amount' | xargs printf '%0.2f')
-		
+
 		oldprice="0.00"
 		delta="0.00"
 
-		if [ -f $pricefile ]; then
+		if [[ -f $pricefile ]]; then
 			oldprice="$(cat $pricefile)"
 			delta=$(echo "$price - $oldprice" | bc | xargs printf '%0.2f')
 		fi
 
-		if [ 1 -eq "$(echo "${price} > ${oldprice}" | bc)" ] && [ "$delta" != "0.00" ]; then
+		if [[ 1 -eq "$(echo "${price} > ${oldprice}" | bc)" ]] && [[ "$delta" != "0.00" ]]; then
 			echo -n $green > $trendfile
-		elif [ 1 -eq "$(echo "${price} < ${oldprice}" | bc)" ]; then
+		elif [[ 1 -eq "$(echo "${price} < ${oldprice}" | bc)" ]]; then
 			echo -n $red > $trendfile
 		else
 			echo -n $yellow > $trendfile
 		fi
 
 		# add + to positive deltas
-		if [ "$delta" != "0.00" ] && [ "${delta:0:1}" != "-" ]; then
+		if [[ "$delta" != "0.00" ]] && [[ "${delta:0:1}" != "-" ]]; then
 			delta="+$delta"
 		fi
 
 		echo -n $price > $pricefile
-		echo -n "$price USD | $delta" > $conkyfile
+		
+		out="$price USD | $delta"
+		echo $out
+		echo -n $out > $conkyfile
 
 	else
 		echo "Price not available"
