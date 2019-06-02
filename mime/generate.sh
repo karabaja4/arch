3ce374d1de27fc1e2e10de19e547442e1f9598f3
karabaja4 <karabaja4@gmail.com>
2019-06-02 04:59:58 +0200

    a

--- mime/generate.sh
+++ mime/generate.sh
@@ -3,10 +3,11 @@
 declare dir="$(dirname $(readlink -f $0))"
 declare path="$dir/mimeapps.list"
 
-rm "$path"
+rm -f "$path"
 cat /usr/share/mime/globs | cut -d':' -f 1 >> "$path"
 cat /usr/share/mime/aliases | cut -d' ' -f 1 >> "$path"
 cat /usr/share/mime/aliases | cut -d' ' -f 2 >> "$path"
+echo -e "application/octet-stream" >> "$path"
 sort -u -o "$path" "$path"
 sed -e 's/$/=mymime.desktop;/' -i "$path"
 sed -i '1s/^/[Default Applications]\n/' "$path"
