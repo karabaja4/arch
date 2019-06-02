87a6ff06638c760fbf4f5857b3c359c88714c9a6
karabaja4 <karabaja4@gmail.com>
2019-06-02 03:42:34 +0200

    a

--- mime/mymime.sh
+++ mime/mymime.sh
@@ -10,7 +10,7 @@ case "$extension" in
         gpicview "$path";;
     avi|mkv|flac|mp3|wav)
         vlc "$path";;
-    txt|c|js|conf|md|sh)
+    txt|c|js|conf|md|sh|json|map)
         geany -i -m -n -p -s -t "$path";;
     *)
         zenity --info --text="No extension was defined for $extension" --no-wrap;;
