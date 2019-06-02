b617c0f2c6576629954ed65008e1c209b4b5c8f4
karabaja4 <karabaja4@gmail.com>
2019-06-02 04:22:08 +0200

    a

---
+++ mime/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare dir="$(dirname $(readlink -f $0))"
+declare path="$dir/mimeapps.list"
+
+rm "$path"
+cat /usr/share/mime/globs | cut -d':' -f 1 >> "$path"
+cat /usr/share/mime/aliases | cut -d' ' -f 1 >> "$path"
+cat /usr/share/mime/aliases | cut -d' ' -f 2 >> "$path"
+sort -u -o "$path" "$path"
+sed -e 's/$/=mymime.desktop;/' -i "$path"
+sed -i '1s/^/[Default Applications]\n/' "$path"
+sed -i '/^#/d' "$path"
+chmod 400 $path
