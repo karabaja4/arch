93333eedefc77074db0c297bac6c382fa1f81181
karabaja4 <karabaja4@gmail.com>
2019-09-06 04:01:58 +0200

    a

--- scripts/print.sh
+++ scripts/print.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -euo pipefail
 
-mutt -s "hpeprint" -a "$1" < /dev/null -- karabaja4@hpeprint.com
+curl -d "{\"to\":\"karabaja4@hpeprint.com\",\"subject\":\"hpeprint\",\"body\":\"print\",\"attachment\":{\"fileName\":\"file.pdf\",\"bytesBase64\":\"$(cat $1 | base64)\",\"mimeType\":\"application/pdf\"}}" -H "Content-Type: application/json" -X POST https://mail.aerium.hr/send
