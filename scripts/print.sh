8058f7925b85f11625d286577a9f45e2eac08243
karabaja4 <karabaja4@gmail.com>
2019-09-06 10:15:51 +0200

    a

--- scripts/print.sh
+++ scripts/print.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -d "{\"to\":\"karabaja4@hpeprint.com\",\"subject\":\"hpeprint\",\"body\":\"print\",\"attachment\":{\"fileName\":\"file.pdf\",\"bytesBase64\":\"$(cat $1 | base64)\",\"mimeType\":\"application/pdf\"}}" -H "Content-Type: application/json" -X POST https://mail.aerium.hr/send
+curl -v -d "{\"to\":\"karabaja4@hpeprint.com\",\"subject\":\"hpeprint\",\"body\":\"print\",\"attachment\":{\"fileName\":\"file.pdf\",\"bytesBase64\":\"$(cat $1 | base64)\",\"mimeType\":\"application/pdf\"}}" -H "Content-Type: application/json" -H "Authorization: Bearer $(cat /home/igor/arch/scripts/secret.json | jq -r '.EmailToken')" -X POST https://mail.aerium.hr/send
