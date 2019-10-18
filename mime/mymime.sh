2c8b67345e4763f69682d9c7428793ceaa7d1e5e
karabaja4 <karabaja4@gmail.com>
2019-10-18 05:26:55 +0200

    a

--- mime/mymime.sh
+++ mime/mymime.sh
@@ -5,34 +5,27 @@ declare extension=""
 declare path="$1"
 declare filename=$(basename "$path")
 
-echo "Opening: $path"
-echo "Base file name: $filename"
-
-if [[ ${filename:0:1} == "." ]]; then
-    echo "Dotfile detected, not using extensions"
-elif [[ $filename != *"."* ]]; then
-    echo "Filename does not contain a dot, not using extensions"
-else
+# if not a dotfile and contains a dot, get the extension
+if [[ ${filename:0:1} != "." ]] && [[ $filename == *"."* ]]
+then
     extension="${filename##*.}"
-    echo "Found extension: $extension"
 fi
 
 case "$extension" in
     pdf)
         mupdf -r 96 "$path";;
-    jpg|jpeg|svg|png|bmp)
+    jpg|jpeg|svg|png|bmp|gif|tga)
         gpicview "$path";;
     avi|mkv|flac|mp3|wav|mp4)
         vlc "$path";;
-    txt|c|js|conf|md|sh|json|map|yml|xml|py|log)
+    txt|c|js|conf|md|sh|json|map|yml|xml|py|log|cs)
         geany -i -m -n -p -s -t "$path";;
     torrent)
         qbittorrent "$path";;
     html|htm)
         chromium "$path";;
     *)
-        echo "Using xdg-mime to query filetype for file $path"
-        mime=$(xdg-mime query filetype "$path")
+        mime=$(file --brief --mime-type "$path")
         case "$mime" in
             text/plain)
                 geany -i -m -n -p -s -t "$path";;
