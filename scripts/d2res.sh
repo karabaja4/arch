404ab7b7a8454508c49c5ad18a9b8095aa0226a9
karabaja4 <karabaja4@gmail.com>
2020-04-07 05:25:55 +0200

    a

--- scripts/d2res.sh
+++ scripts/d2res.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-declare monitor="HDMI-1-1"
+declare monitor1="HDMI-1-1"
+declare monitor2="DP-1-1"
 declare width=1280
 declare height=800
 declare refresh=60
 
 xrandr --newmode $(cvt ${width} ${height} ${refresh} | grep Modeline | sed -e 's/^Modeline //' | sed -e 's/\"//g')
-xrandr --addmode ${monitor} ${width}x${height}_${refresh}.00
-xrandr --output ${monitor} --mode ${width}x${height}_${refresh}.00 --primary
+
+xrandr --addmode ${monitor1} ${width}x${height}_${refresh}.00
+xrandr --addmode ${monitor2} ${width}x${height}_${refresh}.00
+
+xrandr --output ${monitor1} --mode ${width}x${height}_${refresh}.00 --primary
+xrandr --output ${monitor2} --mode ${width}x${height}_${refresh}.00 --right-of ${monitor1}
