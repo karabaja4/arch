79730475047cea89324aac1933a2b48c0d42d85d
karabaja4 <karabaja4@gmail.com>
2020-08-21 03:12:11 +0200

    a

--- gdax/bitmex.js
+++ gdax/bitmex.js
@@ -1,50 +1,40 @@
 const fs = require('fs');
-const WebSocket = require("ws");
+const WebSocket = require('ws');
 
-//const yellow = "#EEFF41";
-const green = "#69F0AE";
-const red = "#FF6E40";
-//const gray = "#757575";
+const green = '#69F0AE';
+const red = '#FF6E40';
 
-const connect = () =>
-{
-    const ws = new WebSocket("wss://www.bitmex.com/realtime");
-    ws.on("open", () =>
-    {
-        const instrument = JSON.stringify({ op: "subscribe", args: "instrument:XBTUSD" });
-        ws.send(instrument);
-    });
-    
-    ws.on("message", (data) =>
-    {
-        const parsed = JSON.parse(data);
-        if (parsed.table == "instrument" && parsed.action == "update")
-        {
-            const info = parsed.data[0];
-            if (info && info.lastPrice)
-            {
-                const direction = info.lastTickDirection == "ZeroPlusTick" || info.lastTickDirection == "PlusTick" ? 1 : -1;
-                const color = direction == 1 ? green : red;
-                const price = parseInt(info.lastPrice);
-                const text = `${price} USD`;
-                console.log(text);
-                fs.writeFile("/tmp/btctrend", color, () => {});
-                fs.writeFile("/tmp/btcconky", text, () => {});
-            }
+const connect = () => {
+  const ws = new WebSocket('wss://www.bitmex.com/realtime');
+  ws.on('open', () => {
+    const instrument = JSON.stringify({ op: 'subscribe', args: 'instrument:XBTUSD' });
+    ws.send(instrument);
+  });
+  
+  ws.on('message', async (data) => {
+    try {
+      const parsed = JSON.parse(data);
+      if (parsed.table == 'instrument' && parsed.action == 'update') {
+        const info = parsed.data[0];
+        if (info && info.lastPrice) {
+          const direction = info.lastTickDirection == 'ZeroPlusTick' || info.lastTickDirection == 'PlusTick' ? 1 : -1;
+          const color = direction == 1 ? green : red;
+          const price = parseInt(info.lastPrice);
+          const text = `${price} USD`;
+          console.log(`${color} ${text}`);
+          await fs.promises.writeFile('/tmp/btctrend', color);
+          await fs.promises.writeFile('/tmp/btcconky', text);
         }
-    });
-    
-    // Close always gets called after error. This will call connect twice on an error.
-    // ws.on('error', () =>
-    // {
-    //     console.log('socket error');
-    // });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
 
-    ws.on("close", () =>
-    {
-        console.log("closed, reconnecting...");
-        setTimeout(connect, 1000);
-    });
+  ws.on('close', () => {
+    console.log('closed, reconnecting...');
+    setTimeout(connect, 1000);
+  });
 };
 
 connect();
