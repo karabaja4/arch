7a1350eb579a166d7c83c635b82d1fc6a754a7c6
karabaja4 <karabaja4@gmail.com>
2020-08-27 23:54:16 +0200

    a

--- gdax/tv/tv.js
+++ gdax/tv/tv.js
@@ -1,9 +1,8 @@
 
-const wsc = require('websocket').client;
-const ws = new wsc();
+const WebSocket = require('ws');
 const rs = require('randomstring');
 
-const symbols = [
+const sym = [
   'BITMEX:XBTUSD',
   'TVC:USOIL',
   'TVC:DXY',
@@ -22,59 +21,75 @@ const symbols = [
   'NYSE:CRM'
 ];
 
-const isDataObject = (obj) => {
-  return obj.p && 
-         Array.isArray(obj.p) &&
-         obj.p[1] && 
-         obj.p[1]['n'] && 
-         obj.p[1]['s'] && 
-         obj.p[1]['v'] && 
-         obj.p[1]['v']['lp'];
+const isDataObject = (o) => {
+  return o.p && 
+         Array.isArray(o.p) &&
+         o.p[1] && 
+         o.p[1]['n'] && 
+         o.p[1]['s'] && 
+         o.p[1]['v'] && 
+         o.p[1]['v']['lp'];
 }
 
-const process = (message, conn) => {
-  const sessionId = `qs_${rs.generate(12)}`;
-  if (message.includes('session_id')) {
-    conn.sendUTF(`~m~52~m~{"m":"quote_create_session","p":["${sessionId}"]}`);
-    conn.sendUTF(`~m~305~m~{"m":"quote_add_symbols","p":["${sessionId}","${symbols.join('","')}",{"flags":["force_permission"]}]}`);
-  } else {
-    if (message.match(/^~m~\d+~m~~h~\d+$/g)) {
-      conn.sendUTF(message);
-      console.log('pong');
-    } else {
-      const parts = message.split('~m~');
-      parts.forEach(x => {
-        const match = x.match(/{.+}/g);
-        if (match) {
-          try {
-            const parsed = JSON.parse(match[0]);
-            if (isDataObject(parsed)) {
-              const name = parsed.p[1]['n'];
-              const price = parsed.p[1]['v']['lp'];
-              const change = parsed.p[1]['v']['ch'];
-              const percent = parsed.p[1]['v']['chp'];
-
-              console.log(`${name} -> PRICE: ${price}, ${change ? `CHANGE: ${change}, ` : ''}${percent ? `PERCENT: ${percent}%` : ''}`);
-            }
-          } catch(e) {
-            console.log(`error parsing json: ${match[0]}`);
-            console.log(e);
-          }
+const process = (message) => {
+  const parts = message.split('~m~');
+  parts.forEach(p => {
+    const match = p.match(/{.+}/g);
+    if (match) {
+      try {
+        const parsed = JSON.parse(match[0]);
+        if (isDataObject(parsed)) {
+          const name = parsed.p[1]['n'];
+          const price = parsed.p[1]['v']['lp'];
+          const change = parsed.p[1]['v']['ch'];
+          const percent = parsed.p[1]['v']['chp'];
+          console.log(`${name} -> PRICE: ${price}${change ? `, CHANGE: ${change}` : ''}${percent ? `, PERCENT: ${percent}%` : ''}`);
         }
-      });
+      } catch(e) {
+        console.log(`error parsing json: ${match[0]}`);
+        console.log(e);
+      }
     }
-  }
+  });
 }
 
-ws.on('connect', (conn) => {
-  conn.on('close', () => {
-    console.log('close');
+const connect = () => {
+
+  const ws = new WebSocket('wss://data.tradingview.com/socket.io/websocket', {
+    origin: 'https://www.tradingview.com'
   });
-  conn.on('message', (message) => {
-    if (conn.connected) {
-      process(message.utf8Data, conn);
+
+  ws.on('open', () => {
+    console.log('open');
+  });
+  
+  ws.on('message', (message) => {
+    if (message.includes('session_id')) {
+      const sid = `qs_${rs.generate(12)}`;
+      const messages = [
+        `~m~52~m~{"m":"quote_create_session","p":["${sid}"]}`,
+        `~m~305~m~{"m":"quote_add_symbols","p":["${sid}","${sym.join('","')}",{"flags":["force_permission"]}]}`
+      ];
+      messages.forEach(m => ws.send(m));
+    } else {
+      if (message.match(/^~m~\d+~m~~h~\d+$/g)) { // ping
+        ws.send(message);
+        console.log('pong');
+      } else {
+        process(message);
+      }
     }
   });
-})
 
-ws.connect('wss://data.tradingview.com/socket.io/websocket', null, 'https://www.tradingview.com');
+  ws.on('error', () => {
+    console.log('socket error');
+  });
+
+  ws.on('close', () => {
+    console.log('closed, reconnecting...');
+    setTimeout(connect, 5000);
+  });
+
+};
+
+connect();
