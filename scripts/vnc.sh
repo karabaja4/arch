c26f6b5e5b1482dab2afded6fb6af89a2da7bbda
karabaja4 <karabaja4@gmail.com>
2020-08-30 18:12:25 +0200

    a

---
+++ scripts/vnc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare -r VNCDIR="/home/igor/.vnc"
+declare -r PIDDIR="/tmp/minirc"
+
+echo "clearing old PIDs..."
+rm -f $VNCDIR/*.pid
+rm -f $VNCDIR/*.log
+
+echo "starting server..."
+/usr/bin/vncserver
+
+echo "copying PID..."
+cp $VNCDIR/*.pid $PIDDIR/vnc.pid
