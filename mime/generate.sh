263d9c51f1c24661b7a01167e107bbaa249dae26
karabaja4 <karabaja4@gmail.com>
2020-08-31 22:27:20 +0200

    a

--- mime/generate.sh
+++ mime/generate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-declare dir="$(dirname $(readlink -f $0))"
-declare path="$dir/mimeapps.list"
+declare dir="$(dirname "$(readlink -f "${0}")")"
+declare path="${dir}/mimeapps.list"
 
-rm -f "$path"
-cat /usr/share/mime/globs | cut -d':' -f 1 >> "$path"
-cat /usr/share/mime/aliases | cut -d' ' -f 1 >> "$path"
-cat /usr/share/mime/aliases | cut -d' ' -f 2 >> "$path"
-cat /usr/share/mime/types >> "$path"
-cat /usr/share/mime/packages/freedesktop.org.xml | grep "<mime-type type=" | cut -d \" -f2 >> "$path"
-sort -u -o "$path" "$path"
-sed -e 's/$/=mymime.desktop;/' -i "$path"
-sed -i '1s/^/[Default Applications]\n/' "$path"
-sed -i '/^#/d' "$path"
-chmod 400 $path
+rm -f "${path}"
+cat /usr/share/mime/globs | cut -d':' -f 1 >> "${path}"
+cat /usr/share/mime/aliases | cut -d' ' -f 1 >> "${path}"
+cat /usr/share/mime/aliases | cut -d' ' -f 2 >> "${path}"
+cat /usr/share/mime/types >> "${path}"
+cat /usr/share/mime/packages/freedesktop.org.xml | grep "<mime-type type=" | cut -d \" -f2 >> "${path}"
+sort -u -o "${path}" "${path}"
+sed -e 's/$/=mymime.desktop;/' -i "${path}"
+sed -i '1s/^/[Default Applications]\n/' "${path}"
+sed -i '/^#/d' "${path}"
+chmod 400 ${path}
