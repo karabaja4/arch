b2ff7b71d13433061986497bd1b66eac265aa774
karabaja4 <karabaja4@gmail.com>
2020-08-31 21:58:37 +0200

    a

--- qtfm/qtfm.sh
+++ qtfm/qtfm.sh
@@ -3,72 +3,72 @@
 
 declare -r action_path="/tmp/qtfm/action"
 declare -r files_path="/tmp/qtfm/files"
-declare -r base_dir="$(dirname "$(readlink -f "$0")")"
+declare -r base_dir="$(dirname "$(readlink -f "${0}")")"
 
 spin() {
-    zenity --progress --pulsate --no-cancel --auto-close --text="$1"
+    zenity --progress --pulsate --no-cancel --auto-close --text="${1}"
 }
 
-case "$1" in
+case "${1}" in
 cut)
-    echo -n "mv" > "$action_path"
-    printf "%s\n" "${@:2}" > "$files_path"
+    echo -n "mv" > "${action_path}"
+    printf "%s\n" "${@:2}" > "${files_path}"
     ;;
 copy)
-    echo -n "cp -r" > "$action_path"
-    printf "%s\n" "${@:2}" > "$files_path"
+    echo -n "cp -r" > "${action_path}"
+    printf "%s\n" "${@:2}" > "${files_path}"
     ;;
 paste)
-    if [ ! -f "$action_path" ] || [ ! -f "$files_path" ]
+    if [ ! -f "${action_path}" ] || [ ! -f "${files_path}" ]
     then
         exit 1
     fi
-    declare -r action="$(cat "$action_path")"
+    declare -r action="$(cat "${action_path}")"
     while IFS= read -r line
     do
-        if [ ! -e "$line" ] # check if source exists
+        if [ ! -e "${line}" ] # check if source exists
         then
-            zenity --error --no-wrap --text="$line does not exist"
+            zenity --error --no-wrap --text="${line} does not exist"
         else
-            declare dest="$2/$(basename "$line")"
-            if [ -e "$dest" ] # check if dest exists
+            declare dest="${2}/$(basename "${line}")"
+            if [ -e "${dest}" ] # check if dest exists
             then
-                declare suffix="$(ls "$dest"* | wc -l)"
+                declare suffix="$(ls "${dest}"* | wc -l)"
                 dest="${dest}_${suffix}"
             fi
-            declare cmd=($action)
-            "${cmd[@]}" "$line" "$dest" | spin "$action $line to $dest"
+            declare cmd=(${action})
+            "${cmd[@]}" "${line}" "${dest}" | spin "${action} ${line} to ${dest}"
         fi
-    done < "$files_path"
+    done < "${files_path}"
     ;;
 rm)
     rm -rf "${@:2}";;
 copypath)
-    echo -n "$2" | xclip -i -selection clipboard;;
+    echo -n "${2}" | xclip -i -selection clipboard;;
 extract)
-    tar xvf "$2" | spin "extracting $2";;
+    tar xvf "${2}" | spin "extracting ${2}";;
 term)
-    xfce4-terminal --working-directory="$2";;
+    xfce4-terminal --working-directory="${2}";;
 feh)
-    feh --bg-scale "$2";;
+    feh --bg-scale "${2}";;
 thumb)
-    convert -resize 13% "$2" "thumb_$2";;
+    convert -resize 13% "${2}" "thumb_${2}";;
 unzip)
-    unzip "$2" | spin "extracting $2";;
+    unzip "${2}" | spin "extracting ${2}";;
 unrar)
-    unrar x "$2" | spin "extracting $2";;
+    unrar x "${2}" | spin "extracting ${2}";;
 gzip)
-    tar cvzf "$2.tar.gz" "${@:3}" | spin "creating archive $2.tar.gz";;
+    tar cvzf "${2}.tar.gz" "${@:3}" | spin "creating archive ${2}.tar.gz";;
 copyurl)
-    declare -r dir="$(cat "$base_dir/store.json" | jq -r ".dir")"
-    if [[ "$2" != "$dir"* ]]
+    declare -r dir="$(cat "${base_dir}/store.json" | jq -r ".dir")"
+    if [[ "${2}" != "${dir}"* ]]
     then
         zenity --error --no-wrap --text="Not a remote file."
         exit 1
     fi
-    declare -r url="$(cat "$base_dir/store.json" | jq -r ".url")"
-    declare -r sas="$(cat "$base_dir/store.json" | jq -r ".sas")"
-    declare -r path="$(realpath --relative-to="$dir" "$2")"
+    declare -r url="$(cat "${base_dir}/store.json" | jq -r ".url")"
+    declare -r sas="$(cat "${base_dir}/store.json" | jq -r ".sas")"
+    declare -r path="$(realpath --relative-to="${dir}" "${2}")"
     echo "${url}${path}${sas}" | xclip -i -selection clipboard
     ;;
 esac
