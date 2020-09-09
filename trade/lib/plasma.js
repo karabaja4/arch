c8802d71e66971bc6f6a6b98f66fc40cd871ad8b
karabaja4 <karabaja4@gmail.com>
2020-09-09 02:43:37 +0200

    a

--- trade/lib/plasma.js
+++ trade/lib/plasma.js
@@ -3,13 +3,20 @@ const figlet = require('figlet');
 const chalk = require('chalk');
 const util = require('util');
 
+const symbols = [
+  "BITMEX:XBTUSD",
+  "TVC:USOIL"
+];
+
 const render = util.promisify(figlet.text);
 
 const print = async () => {
   console.clear();
   const keys = [];
   for(const name in store) {
-    keys.push(name);
+    if (symbols.includes(name)) {
+      keys.push(name);
+    }
   }
   keys.sort();
   for(let i = 0; i < keys.length; i++) {
@@ -19,7 +26,7 @@ const print = async () => {
     const price = `${value.price.toFixed(2)} USD`.padEnd(12);
     const change = `${value.change > 0 ? '+' : ''}${value.change.toFixed(2)} USD`;
 
-    let text = await render(`${name} ${price} | ${change}`, { font: '3x5' });
+    let text = await render(`${name} ${price} | ${change}`, { font: '3x5', width: 1000 });
     text = text.replace(/#/g, '█');
     console.log(chalk[value.change > 0 ? 'green' : 'red'](text));
   }
