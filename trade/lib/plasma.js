eb5e06f3f75f1afb6aa95895b32a5d8902c06562
karabaja4 <karabaja4@gmail.com>
2020-09-11 00:52:01 +0200

    a

--- trade/lib/plasma.js
+++ trade/lib/plasma.js
@@ -17,7 +17,7 @@ const symbols = [
 const render = util.promisify(figlet.text);
 
 const print = async () => {
-  console.clear();
+  process.stdout.write('\x1B[2J\x1B[3J\x1B[H\x1Bc');
   const keys = [];
   for(const name in store) {
     if (symbols.includes(name)) {
