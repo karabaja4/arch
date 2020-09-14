2ebade6661723c59034edfe3ff9255ad8dff0265
karabaja4 <karabaja4@gmail.com>
2020-09-15 01:44:01 +0200

    a

--- text/text.sh
+++ text/text.sh
@@ -7,13 +7,13 @@ then
     declare -r temp="$(date -u +"%Y-%m-%dT%H-%M-%SZ")"
     declare -r filename="${HOME}/.text/${temp}.js"
     touch "${filename}"
-    echo "Opening ${filename}"
+    #echo "Opening ${filename}"
     featherpad "${filename}" &
     # if [ ! -s "${filename}" ]
     # then
     #     rm "${filename}"
     # fi
 else
-    echo "Opening ${1}"
+    #echo "Opening ${1}"
     featherpad "${1}" &
 fi
