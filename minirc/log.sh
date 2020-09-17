2b1ac3271482f9860ff8b1ba633b75cfca6ad15c
karabaja4 <karabaja4@gmail.com>
2020-09-18 01:45:53 +0200

    a

--- minirc/log.sh
+++ minirc/log.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 declare -r pid="$(cat /tmp/minirc/${1}.pid)"
 sudo -u root strace -e write -p "${pid}" -s 10000 |& \
 grep -oP --line-buffered '(?<=write\(1, ").*(?=\\n",)' | \
-awk -v src="${1}" '{ print "["src"]["strftime("%Y-%m-%dT%H:%M:%S")"] "$0; fflush(); }'
+awk -v src="${1}" -v pid="${pid}" '{ print "["pid"]["src"]["strftime("%Y-%m-%dT%H:%M:%S")"] "$0; fflush(); }'
 
