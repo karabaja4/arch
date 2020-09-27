5047840926cc780a2f00f5089d81872d8f2971da
karabaja4 <karabaja4@gmail.com>
2020-09-28 00:42:35 +0200

    a

--- scripts/dns.sh
+++ scripts/dns.sh
@@ -1,41 +1,25 @@
 #!/bin/bash
-set -euo pipefail
+set -u
 
 declare -r basedir="$(dirname "$(readlink -f "${0}")")"
 declare -r token="$(cat "${basedir}/secret.json" | jq -r ".DNSToken")"
 declare -r url="https://api.digitalocean.com/v2/domains/aerium.hr/records/53478297"
 declare -ar headers=("-H" "Content-Type: application/json" "-H" "Authorization: Bearer ${token}")
+declare ip=""
+declare dns=""
 
-log () {
-    echo "[$(date -Is)]: ${1}"
+end () {
+    echo "[$(date -Is)]: ${1} (${2})"
+    exit ${2}
 }
 
-declare -r ip="$(curl -s -f "https://api.ipify.org")"
-if [ ${?} -ne 0 ]
+ip="$(curl -s -f "https://api.ipify.org")" || end "IP request failed." ${?} 
+dns="$(curl -s -f -X GET "${headers[@]}" "${url}" | jq -r ".domain_record .data")" || end "DigitalOcean GET domain failed." ${?} 
+
+if [ "${ip}" == "${dns}" ]
 then
-    log "IP request failed."
-    exit 1
+    log "No update necessary."
 else
-    declare -r dns="$(curl -s -f -X GET "${headers[@]}" "${url}" | jq -r ".domain_record .data")"
-    if [ ${?} -ne 0 ]
-    then
-        log "DigitalOcean GET domain failed."
-        exit 1
-    else
-        if [ "${ip}" == "${dns}" ]
-        then
-            log "No update necessary."
-            exit 0
-        else
-            curl -s -f -o /dev/null -X PUT "${headers[@]}" -d "{\"data\":\"${ip}\"}" "${url}"
-            if [ $? -ne 0 ]
-            then
-                log "DigitalOcean PUT domain failed."
-                exit 1
-            else
-                log "Update successful: ${ip}"
-                exit 0
-            fi
-        fi
-    fi
+    curl -s -f -o /dev/null -X PUT "${headers[@]}" -d "{\"data\":\"${ip}\"}" "${url}" || end "DigitalOcean PUT domain failed." ${?}
+    log "Update successful: ${ip}"
 fi
