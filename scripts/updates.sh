18137524dd6778ddae891e815f9fcfd3290d75db
karabaja4 <karabaja4@gmail.com>
2020-09-28 01:51:02 +0200

    a

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -6,12 +6,12 @@ declare -i rv=0
 while true
 do
     checkupdates | wc -l > /tmp/update_count
-    rv=$?
-    if [ $rv -eq 0 ] || [ $rv -eq 2 ]
+    rv=${?}
+    if [ ${rv} -eq 0 ] || [ ${rv} -eq 2 ]
     then
-        echo "success ($rv)"
+        echo "success (${rv})"
         break
     fi
-    echo "retrying ($rv)..."
+    echo "retrying (${rv})..."
     sleep 1
 done
