dca3d67d1c93eff050a241ae35fba827d4766472
karabaja4 <karabaja4@gmail.com>
2020-09-28 01:02:10 +0200

    a

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-declare -r countfile="/tmp/update_count"
-
-[ ! -f "${countfile}" ] && echo "-" > "${countfile}"
-
-while ! ping -q -c 1 -W 1 www.google.com &> /dev/null
+while ! checkupdates | wc -l > /tmp/update_count
 do
     sleep 1
 done
 
-checkupdates | wc -l > "${countfile}"
