cb8e4c56e9fe16a93fbf9d4e559aee72b95272d3
karabaja4 <karabaja4@gmail.com>
2020-09-29 03:15:45 +0200

    a

--- text/text.sh
+++ text/text.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-set -euo pipefail
 
-if [ $# -eq 0 ]
-then
-    mkdir -p "${HOME}/.text"
-    declare -r temp="$(date -u +"%Y-%m-%dT%H-%M-%SZ")"
-    declare -r filename="${HOME}/.text/${temp}.js"
-    featherpad "${filename}" &> /dev/null &
-else
-    featherpad "${1}" &> /dev/null &
-fi
+# sudo ln -s /home/igor/arch/text/text.sh /usr/bin/ed
+
+featherpad "${1}" &> /dev/null &
