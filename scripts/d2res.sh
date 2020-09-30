dd0bac79428393d99e7967f4bcac5453cc417ee4
karabaja4 <karabaja4@gmail.com>
2020-09-30 07:02:10 +0200

    a

--- scripts/d2res.sh
+++ scripts/d2res.sh
@@ -9,7 +9,6 @@ declare refresh=60
 xrandr --newmode $(cvt ${width} ${height} ${refresh} | grep Modeline | sed -e 's/^Modeline //' | sed -e 's/\"//g')
 
 xrandr --addmode ${monitor1} ${width}x${height}_${refresh}.00
-xrandr --addmode ${monitor2} ${width}x${height}_${refresh}.00
 
 xrandr --output ${monitor1} --mode ${width}x${height}_${refresh}.00 --primary
-xrandr --output ${monitor2} --mode ${width}x${height}_${refresh}.00 --right-of ${monitor1}
+xrandr --output ${monitor2} --right-of ${monitor1}
