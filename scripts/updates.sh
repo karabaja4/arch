f5cda038e32f64cb1f55436f70df32f37c898702
karabaja4 <karabaja4@gmail.com>
2020-10-01 18:14:48 +0200

    a

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -uo pipefail
 
+declare path="/tmp/update_count"
+
+if [ ! -f "${path}" ]
+then
+    install -m 666 /dev/null "${path}"
+fi
+
 declare -i rv=0
 declare result=""
 
@@ -8,10 +15,10 @@ while true
 do
     result="$(checkupdates | wc -l)"
     rv=${?}
-    if [[ ( ${rv} -eq 0 || ${rv} -eq 2 ) && ( ${result} != "" ) ]]
+    if [ ${rv} -eq 0 ] || [ ${rv} -eq 2 ]
     then
         echo "success (${rv})"
-        echo "${result}" > /tmp/update_count
+        echo "${result}" > "${path}"
         break
     fi
     echo "retrying (${rv})..."
