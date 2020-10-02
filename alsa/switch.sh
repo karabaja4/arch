a98b2ede7bc163abee9b83942e453a92d54fec55
karabaja4 <karabaja4@gmail.com>
2020-10-02 02:30:04 +0200

    a

--- alsa/switch.sh
+++ alsa/switch.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+set -euo pipefail
 
-if [ "${1}" != "speakers" ] && [ "${1}" != "headset" ]
-then
-    echo "wrong parameter"
+usage() {
+    echo "usage: switch.sh [speakers|headset|master]"
     exit 1
-fi
+}
+
+[ ${#} -eq 0 ] && usage
 
 declare -r basedir="$(dirname "$(readlink -f "${0}")")"
-ln -sf "${basedir}/asoundrc.${1}" "${HOME}/.asoundrc"
-alsactl -f "${basedir}/asound.state.${1}" restore
+
+case "${1}" in
+speakers)
+    ln -sf "${basedir}/asoundrc.speakers" "${HOME}/.asoundrc"
+    amixer set Master unmute > /dev/null
+    amixer set Master 100% > /dev/null
+    ;;
+headset)
+    ln -sf "${basedir}/asoundrc.headset" "${HOME}/.asoundrc"
+    amixer set Headphone unmute > /dev/null
+    amixer set Headphone 100% > /dev/null
+    amixer set Mic unmute > /dev/null
+    amixer set Mic 100% > /dev/null
+    ;;
+master)
+    amixer set Master unmute > /dev/null
+    amixer set Master 100% > /dev/null
+    ;;
+*)
+    usage
+    ;;
+esac
