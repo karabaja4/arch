af063579a7b5a2316ad3521c31adaa17ceff6559
karabaja4 <karabaja4@gmail.com>
2020-10-02 02:39:01 +0200

    a

--- scripts/old/share.sh
+++ scripts/old/share.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo mount -t cifs -o username="root",password="$1",file_mode=0777,dir_mode=0777 //20.0.0.1/shared /home/igor/shared/
+
+sudo mount -t cifs //192.168.0.10/storage _nas/ -o username=root,password=password,vers=1.0,uid=$(id -u),gid=$(id -g)
