eb2d8a5c010d219992ef010d35b4f016f9bc11ca
karabaja4 <karabaja4@gmail.com>
2020-10-24 14:16:00 +0200

    a

--- alsa/switch.sh
+++ alsa/switch.sh
@@ -8,16 +8,18 @@ usage() {
 
 [ ${#} -eq 0 ] && usage
 
-declare -r basedir="$(dirname "$(readlink -f "${0}")")"
+asoundrc() {
+    echo -e "defaults.ctl.card ${1}\ndefaults.pcm.card ${1}\ndefaults.pcm.device 0" > "${HOME}/.asoundrc"
+}
 
 case "${1}" in
 speakers)
-    ln -sf "${basedir}/asoundrc.speakers" "${HOME}/.asoundrc"
+    asoundrc "$(cat /proc/asound/cards | grep PCH | head -n1 | cut -c2)"
     amixer set Master unmute > /dev/null
     amixer set Master 100% > /dev/null
     ;;
 headphones)
-    ln -sf "${basedir}/asoundrc.headphones" "${HOME}/.asoundrc"
+    asoundrc "$(cat /proc/asound/cards | grep Headset | head -n1 | cut -c2)"
     amixer set Headphone unmute > /dev/null
     amixer set Headphone 100% > /dev/null
     amixer set Mic unmute > /dev/null
