280fbbbd52c72df710da86c7845b2990cecd2956
karabaja4 <karabaja4@gmail.com>
2020-11-02 03:07:56 +0100

    a

--- mime2/lib/log.js
+++ mime2/lib/log.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const path = require('path');
 
 const logdir = path.join(os.homedir(), '.local/share/mimejs');
-const logfile = 'mimejs.log';
+const logfile = path.join(logdir, 'mimejs.log');
 
 const write = async (tag, msg) => {
   await fs.promises.mkdir(logdir, { recursive: true });
-  await fs.promises.appendFile(path.join(logdir, logfile), `[${(new Date()).toISOString()}][${tag}]: ${msg}\n`);
+  await fs.promises.appendFile(logfile, `[${(new Date()).toISOString()}][${tag}]: ${msg}\n`);
 };
 
 module.exports = {
