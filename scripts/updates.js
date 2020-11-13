fcd1a95f5cfa49373712ea623a94ec0bf0388312
karabaja4 <karabaja4@gmail.com>
2020-11-13 01:40:41 +0100

    a

--- scripts/updates.js
+++ scripts/updates.js
@@ -1,6 +1,11 @@
 const util = require('util');
 const fs = require('fs');
 const exec = util.promisify(require('child_process').exec);
+const sleep = util.promisify(setTimeout);
+
+const log = async (tag, msg) => {
+  console.log(`[${(new Date()).toISOString()}][${tag}]: ${msg}`);
+};
 
 const write = async (text) => {
   const filepath = "/tmp/update_count"
@@ -8,18 +13,29 @@ const write = async (text) => {
   await fs.promises.chmod(filepath, 0o666);
 }
 
-const main = async () => {
-  let code = 0;
-  try {
-    const result = await exec('checkupdates');
-    const count = result.stdout.trim().split(/\r\n|\r|\n/).length;
-    write(count);
-  } catch (e) {
-    const zero = e.code === 2 && !e.stdout && !e.stderr;
-    write(zero ? '0' : '-');
-    code = e.code;
+const run = async () => {
+  while (true) {
+    try {
+      const result = await exec('checkupdates');
+      const count = result.stdout.trim().split(/\r\n|\r|\n/).length;
+      write(count);
+      return 0;
+    } catch (e) {
+      // exit code 2 means no packages to update
+      if (e.code === 2 && !e.stdout && !e.stderr) {
+        write(0);
+        return e.code;
+      }
+      write('-');
+      log('error', `failed (${e.code}), retrying`);
+      await sleep(1000);
+    }
   }
-  console.log(`exited (${code})`);
+}
+
+const main = async () => {
+  const code = await run();
+  log('info', `exited (${code})`);
 }
 
 main();
