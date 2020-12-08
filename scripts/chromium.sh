2ebfaf94b7bed2c6f0691b237c13e11703cd9fc2
karabaja4 <karabaja4@gmail.com>
2020-12-08 04:15:23 +0100

    a

--- scripts/chromium.sh
+++ scripts/chromium.sh
@@ -4,7 +4,7 @@ declare running="$(wmctrl -lx | grep chromium.Chromium)"
 
 if [ -z "$running" ]
 then
-      killall -q -w chromium
+      killall -q -w chromium | zenity --progress --pulsate --no-cancel --auto-close --text="Waiting for previous Chromium instance to close"
 else
       echo "Found window: $running"
 fi
