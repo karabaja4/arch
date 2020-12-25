1ce0b994e12fbf7f04e2054eafb98fca0aea5fa6
karabaja4 <karabaja4@gmail.com>
2020-12-25 06:16:23 +0100

    a

---
+++ scripts/ed.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# sudo ln -sf /home/igor/arch/scripts/ed.sh /usr/bin/ed
+
+leafpad "${1}" &> /dev/null &
--- text/text.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# sudo ln -sf /home/igor/arch/text/text.sh /usr/bin/ed
-
-leafpad "${1}" &> /dev/null &
