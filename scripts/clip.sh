f37ee3792404a106a34edba7678b2c665d04d9af
karabaja4 <karabaja4@gmail.com>
2020-12-31 06:06:20 +0100

    a

---
+++ scripts/clip.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -uo pipefail
+
+echo -e "[$(basename "${0}") - xclip based clipboard manager for XA_CLIPBOARD]"
+
+usage() {
+    echo -e "-> Please define your preferred targets in the 'preferred_targets' array to handle custom formats"
+    exit 1
+}
+
+(( ${#} > 0 )) && usage
+
+declare -ar preferred_targets=(
+    "image/bmp"
+    "image/png"
+    "text/uri-list"
+    "code/file-list"
+    "text/plain"
+)
+
+while true
+do
+    echo "-> $(head -c 78 /dev/zero | tr '\0' '-')"
+    targets=( $(xclip -selection clipboard -o -t TARGETS) )
+
+    echo "-> Preferred targets: ${preferred_targets[@]}"
+    echo "-> Clipboard targets: ${targets[@]}"
+
+    # prints both lists together, and prints intersection sorted by first occurance of type in preferred_targets
+    target="$(echo ${targets[@]} ${preferred_targets[@]} | tr ' ' '\n' | awk 'a[$0]++' | head -n1)"
+    echo "-> Target: ${target:-"text/plain (default)"}"
+
+    xclip -verbose -out -selection clipboard -t "${target:-"text/plain"}" | xclip -verbose -in -selection clipboard -t "${target:-"text/plain"}"
+done
