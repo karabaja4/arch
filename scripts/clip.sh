ba5b62d36e2b9837d4df6a360403d0ad3ab682c7
karabaja4 <karabaja4@gmail.com>
2021-01-01 09:20:35 +0100

    a

--- scripts/clip.sh
+++ scripts/clip.sh
@@ -6,6 +6,7 @@ declare -ar preferred_targets=(
     "image/png"
     "text/uri-list"
     "code/file-list"
+    "UTF8_STRING"
 )
 
 usage() {
@@ -24,13 +25,25 @@ while true
 do
     echo "-> $(head -c 78 /dev/zero | tr '\0' '-')"
     targets=( $(xclip -selection clipboard -o -t TARGETS) )
+    if (( ${?} != 0 || ${#targets[@]} == 0 ))
+    then
+        echo "-> ERROR: failed to fetch targets"
+        sleep 1
+    else
+        echo "-> Preferred targets: ${preferred_targets[@]}"
+        echo "-> Clipboard targets: ${targets[@]}"
 
-    echo "-> Preferred targets: ${preferred_targets[@]}"
-    echo "-> Clipboard targets: ${targets[@]}"
+        # join both lists together, and print first item of targets occuring in preferred_targets
+        target="$(echo ${targets[@]} ${preferred_targets[@]} | tr ' ' '\n' | awk 'a[$0]++' | head -n1)"
 
-    # join both lists together, and print first item of targets occuring in preferred_targets
-    target="$(echo ${targets[@]} ${preferred_targets[@]} | tr ' ' '\n' | awk 'a[$0]++' | head -n1)"
-    echo "-> Target: ${target:-"UTF8_STRING (default)"}"
+        if [[ -n ${target} ]]
+        then
+            echo "-> Chosen target: ${target}"
+            xclip -verbose -out -selection clipboard -t "${target}" | xclip -verbose -in -selection clipboard -t "${target}"
+        else
+            echo "-> ERROR: unable to match targets"
+            sleep 1
+        fi
+    fi
 
-    xclip -verbose -out -selection clipboard -t "${target:-"UTF8_STRING"}" | xclip -verbose -in -selection clipboard -t "${target:-"UTF8_STRING"}"
 done
