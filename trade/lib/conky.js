ee37b212a3ec122d31fc4b483b96f1012a06676b
karabaja4 <karabaja4@gmail.com>
2021-01-11 02:24:39 +0100

    a

--- trade/lib/conky.js
+++ trade/lib/conky.js
@@ -7,7 +7,7 @@ const getName = () => {
   // if (day === 6 || day === 0) {
   //   return 'BITMEX:XBTUSD';
   // }
-  return 'FOREXCOM:NSXUSD';
+  return 'CURRENCYCOM:GOLD';
 };
 
 const write = (key, data) => {
