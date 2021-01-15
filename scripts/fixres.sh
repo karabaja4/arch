738f35a123ead3762b52327c2331117885abd75a
karabaja4 <karabaja4@gmail.com>
2021-01-16 00:13:14 +0100

    a

--- scripts/fixres.sh
+++ scripts/fixres.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # left
-xrandr --output HDMI1 --mode 1920x1200 --primary
+xrandr --output HDMI-1-1 --mode 1920x1200 --primary
 
 # right
-xrandr --addmode DP1 1920x1200
-xrandr --output DP1 --mode 1920x1200 --right-of HDMI1
+xrandr --addmode DP-1-1 1920x1200
+xrandr --output DP-1-1 --mode 1920x1200 --right-of HDMI-1-1
 
 # laptop
-xrandr --output eDP1 --mode 1920x1080 --left-of HDMI1
+xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-1-1
