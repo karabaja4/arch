baeeed250e92792116b7e43c666b8c947a7fb940
karabaja4 <karabaja4@gmail.com>
2021-01-19 01:59:33 +0100

    a

--- scripts/chromium.sh
+++ scripts/chromium.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# shellcheck disable=SC2155,SC2181
 
-killall -q -0 chromium
-if (( ${?} == 0 )) # zero means chromium is running
+if killall -q -0 chromium # is chromium running?
 then
-      declare -r running="$(wmctrl -lx | grep chromium.Chromium)"
-      if [ -z "$running" ]
-      then
-            killall -q -w -0 chromium | zenity --progress --pulsate --no-cancel --auto-close --text="Waiting for previous Chromium instance to close"
-      else
-            echo "Found window: $running"
-      fi
+    if wmctrl -lx | grep chromium.Chromium # is chromium window open?
+    then
+        # chromium window open
+        echo "Found window."
+    else
+        # chromium running but not open
+        killall -q -w -0 chromium | zenity --progress --pulsate --no-cancel --auto-close --text="Waiting for previous Chromium instance to close"
+    fi
 fi
 
 /usr/bin/chromium &> /dev/null
