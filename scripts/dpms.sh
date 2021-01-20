0bce235f500bd56674f47a9ced256ad929cca310
karabaja4 <karabaja4@gmail.com>
2021-01-21 00:19:17 +0100

    a

--- scripts/dpms.sh
+++ scripts/dpms.sh
@@ -4,5 +4,5 @@ if grep -q "RUNNING" /proc/asound/card*/pcm*/sub*/status
 then
     xset -display :0.0 dpms 0 0 0
 else
-    xset -display :0.0 dpms 600 600 600
+    xset -display :0.0 dpms 300 300 300
 fi
