0e05b12cc1ebc74dd9a81b31130ac080438ba4bf
karabaja4 <karabaja4@gmail.com>
2021-01-22 03:03:26 +0100

    a

--- scripts/automount.sh
+++ scripts/automount.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-declare -r _user="igor"
-declare -a _partitions=()
-mapfile -t _partitions <<< "$(lsblk -o name -lnp "${1}")"
-
 _mount() {
+    local -r _user="igor"
     local -r _dir="/mnt/$(basename "${1}")"
     local -r _uid="$(id -u "${_user}")"
     local -r _gid="$(id -g "${_user}")"
@@ -12,10 +9,16 @@ _mount() {
     mount -o uid="${_uid}",gid="${_gid}" "${1}" "${_dir}"
 }
 
-for part in "${_partitions[@]}"
-do
-    if [[ "${part}" != "${1}" ]]
-    then
-        _mount "${part}"
-    fi
-done
+_enum() {
+    local -a _partitions=()
+    mapfile -t _partitions <<< "$(lsblk -o name -lnp "${1}")"
+    for part in "${_partitions[@]}"
+    do
+        if [[ "${part}" != "${1}" ]]
+        then
+            _mount "${part}"
+        fi
+    done
+}
+
+( _enum "${1}" ) &
