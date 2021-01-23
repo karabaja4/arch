f1d4a2816c1958d857cf726ffd49e6fa8a797e4d
karabaja4 <karabaja4@gmail.com>
2021-01-23 05:43:24 +0100

    a

--- scripts/automount.sh
+++ scripts/automount.sh
@@ -4,8 +4,8 @@ set -uo pipefail
 _mount() {
     local -r _user="$(who | awk '{print $1}')"
     local -r _dir="/mnt/$(basename "${1}")"
-    local -r _uid="$(id -u "${_user}")"
-    local -r _gid="$(id -g "${_user}")"
+    local -ir _uid="$(id -u "${_user}")"
+    local -ir _gid="$(id -g "${_user}")"
     install -m 755 -g "${_user}" -o "${_user}" -d "${_dir}"
     mount -o uid="${_uid}",gid="${_gid}" "${1}" "${_dir}"
 }
