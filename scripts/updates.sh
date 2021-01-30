91b3f925a1b4300cad76dc1cc0b9b3e1ede5e808
karabaja4 <karabaja4@gmail.com>
2021-01-30 08:09:11 +0100

    a

---
+++ scripts/updates.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -uo pipefail
+
+_write() {
+    local filepath="/tmp/update_count"
+    echo "${1}" > "${filepath}"
+    chmod 666 "${filepath}"
+    echo "write: ${1} updates"
+}
+
+_run() {
+    local _cu
+    local -i _rv
+    local -i _ec=0
+    while true
+    do
+        _cu="$(checkupdates)"
+        _rv=${?}
+        echo "checkupdates exited with ${_rv}"
+        if (( _rv == 0 || _rv == 2 ))
+        then
+            if [[ -z "${_cu}" ]]
+            then
+                _write "0"
+            else
+                local -a _upd
+                mapfile -t _upd <<< "${_cu}"
+                _write "${#_upd[@]}"
+            fi
+            break
+        else
+            if (( ++_ec >= 100 ))
+            then
+                echo "giving up (${_ec})"
+                break
+            else
+                _write "-"
+                echo "retrying (${_ec})"
+                sleep 1
+            fi
+        fi
+    done
+}
+
+_run
