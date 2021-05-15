41c19e09ade3b2587147925bc46554edcc176c4a
karabaja4 <karabaja4@gmail.com>
2021-05-15 03:35:02 +0200

    a

--- scripts/scrot.sh
+++ scripts/scrot.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+# shellcheck disable=SC2034
+
 set -euo pipefail
+
+_mapfile() {
+    local -n _ref=${1}
+    if [[ -n "${2}" ]]
+    then
+        mapfile -t _ref <<< "${2}"
+    fi
+}
 
 _shot() {
     local -a _resolutions=()
-    mapfile -t _resolutions <<< "$(xrandr | grep -o '[0-9]*x[0-9]*[+-][0-9]*[+-][0-9]*')"
+    _mapfile _resolutions "$(xrandr | grep -o '[0-9]*x[0-9]*[+-][0-9]*[+-][0-9]*')"
     local -i idx=1
     for res in "${_resolutions[@]}"
     do
