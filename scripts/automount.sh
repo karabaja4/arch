a8fd859481967f6f157b04fa04d40c0ac6a7f393
karabaja4 <karabaja4@gmail.com>
2021-05-15 03:33:01 +0200

    a

--- scripts/automount.sh
+++ scripts/automount.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+# shellcheck disable=SC2034
+
 set -uo pipefail
 
+_mapfile() {
+    local -n _ref=${1}
+    if [[ -n "${2}" ]]
+    then
+        mapfile -t _ref <<< "${2}"
+    fi
+}
+
 _mount() {
-    local -r _user="$(who | awk '{print $1}')"
+    local -r _user="$(who | awk 'NR==1{print $1}')"
     local -r _dir="/mnt/$(basename "${1}")"
     local -ir _uid="$(id -u "${_user}")"
     local -ir _gid="$(id -g "${_user}")"
@@ -12,7 +22,7 @@ _mount() {
 
 _enum() {
     local -a _partitions=()
-    mapfile -t _partitions <<< "$(lsblk -o name -lnp "${1}")"
+    _mapfile _partitions "$(lsblk -o name -lnp "${1}")"
     for part in "${_partitions[@]}"
     do
         if [[ "${part}" != "${1}" ]]
