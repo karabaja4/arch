4248acfc144317cbd4482228059e444c2c41ae41
karabaja4 <karabaja4@gmail.com>
2021-06-02 00:15:12 +0200

    a

--- conky/updates.lua
+++ conky/updates.lua
@@ -1,7 +1,7 @@
 function conky_get_updates()
     local file = io.open('/tmp/update_count', 'rb')
     if not file then return '(syncing)' end
-    local content = file:read "*a"
+    local content = file:read '*a'
     file:close()
     return string.gsub(content, '^%s*(.-)%s*$', '%1')
 end
