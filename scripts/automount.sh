7dc9c662ed493d0766e13bab3d637ade35a325aa
karabaja4 <karabaja4@gmail.com>
2021-06-08 01:20:33 +0200

    a

--- scripts/automount.sh
+++ scripts/automount.sh
@@ -18,22 +18,19 @@ declare -ir _id=1000
 
 _mount() {
     local -r _user="$(id -un ${_id})"
-    local -r _dir="/mnt/$(basename "${1}")"
+    local -r _dir="/mnt/${1}"
     local -ir _uid="$(id -u "${_user}")"
     local -ir _gid="$(id -g "${_user}")"
-    install -m 755 -g "${_user}" -o "${_user}" -d "${_dir}"
-    mount -o uid="${_uid}",gid="${_gid}" "${1}" "${_dir}"
+    install -m 0755 -g "${_user}" -o "${_user}" -d "${_dir}"
+    mount -o uid="${_uid}",gid="${_gid}" "/dev/${1}" "${_dir}"
 }
 
 _enum() {
     local -a _partitions=()
-    mapfile -t _partitions < <(lsblk -o name -lnp "${1}")
+    mapfile -t _partitions < <(lsblk -J "${1}" | jq -crM '.blockdevices[] | .children[] | select(.type=="part") | .name')
     for part in "${_partitions[@]}"
     do
-        if [[ "${part}" != "${1}" ]]
-        then
-            _mount "${part}"
-        fi
+        _mount "${part}"
     done
 }
 
