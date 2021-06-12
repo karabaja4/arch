2d99a14b50f122690d521acd5784f300f93e0cc7
karabaja4 <karabaja4@gmail.com>
2021-06-12 15:28:01 +0200

    a

--- scripts/automount.sh
+++ scripts/automount.sh
@@ -1,34 +1,37 @@
-#!/bin/bash
-set -uo pipefail
+#!/bin/sh
+set -u
+
+_echo() {
+    printf '%s\n' "${1}"
+}
 
 _usage() {
-    echo "This is script is called by /etc/udev/rules.d/10-flash.rules"
+    _echo "This is script is called by /etc/udev/rules.d/10-flash.rules"
     exit 2
 }
 
 _not_root() {
-    echo "Root privileges are required to run this command"
+    _echo "Root privileges are required to run this command"
     exit 1
 }
 
-(( ${#} == 0 )) && _usage
-(( EUID != 0 )) && _not_root
+[ "${#}" -ne 1 ] && _usage
+[ "$(id -u)" -ne 0 ] && _not_root
 
-declare -ir _id=1000
+_mount_uid=1000
 
-_mount() {
-    local -r _user="$(id -un ${_id})"
-    local -r _dir="/mnt/${1}"
-    local -ir _uid="$(id -u "${_user}")"
-    local -ir _gid="$(id -g "${_user}")"
+_mount() (
+    _user="$(id -un ${_mount_uid})"
+    _dir="/mnt/${1}"
+    _uid="$(id -u "${_user}")"
+    _gid="$(id -g "${_user}")"
     install -m 0755 -g "${_user}" -o "${_user}" -d "${_dir}"
     mount -o uid="${_uid}",gid="${_gid}" "/dev/${1}" "${_dir}"
-}
+)
 
 _enum() {
-    local -a _partitions=()
-    mapfile -t _partitions < <(lsblk -J "${1}" | jq -crM '.blockdevices[] | .children[] | select(.type=="part") | .name')
-    for part in "${_partitions[@]}"
+    _partitions="$(lsblk -J "${1}" | jq -crM '.blockdevices[] | .children[] | select(.type=="part") | .name')"
+    _echo "${_partitions}" | while read -r part
     do
         _mount "${part}"
     done
