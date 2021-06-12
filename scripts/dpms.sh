a32f97ca3b1b2e56f4ddd5dd226cd8258e9458fc
karabaja4 <karabaja4@gmail.com>
2021-06-12 15:05:40 +0200

    a

--- scripts/dpms.sh
+++ scripts/dpms.sh
@@ -1,26 +1,35 @@
-#!/bin/bash
-# shellcheck disable=SC2155
-set -euo pipefail
+#!/bin/sh
+set -eu
 
-declare -r _current="$(xset -display :0.0 q | awk '/DPMS is/ { print $3 }')"
+_echo() {
+    printf '%s\n' "${1}"
+}
+
+_echo "$(basename "${0}") @ $(readlink /proc/$$/exe)"
+_current="$(xset -display :0.0 q | awk '/DPMS is/ { print $3 }')"
+_echo "DPMS: ${_current}"
 
 _enable() {
-    if [[ "${_current}" == "Disabled" ]]
+    if [ "${_current}" = "Disabled" ]
     then
         xset -display :0.0 +dpms
+        _echo "DPMS: ${_current} -> Enabled"
     fi
 }
 
 _disable() {
-    if [[ "${_current}" == "Enabled" ]]
+    if [ "${_current}" = "Enabled" ]
     then
         xset -display :0.0 -dpms
+        _echo "DPMS: ${_current} -> Disabled"
     fi
 }
 
 if grep -q "RUNNING" /proc/asound/card*/pcm*/sub*/status
 then
+    _echo "ALSA: Running"
     _disable
 else
+    _echo "ALSA: Stopped"
     _enable
 fi
