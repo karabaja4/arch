b4185fef77ad214ef0cb3d819b1668ffc6b36f58
karabaja4 <karabaja4@gmail.com>
2021-06-12 13:57:23 +0200

    a

--- scripts/sound.sh
+++ scripts/sound.sh
@@ -1,35 +1,39 @@
-#!/bin/bash
-set -uo pipefail
+#!/bin/sh
+set -u
+
+_echo() {
+    printf '%s\n' "${1}"
+}
 
 _usage() {
-    echo "usage: ${0} [ <card_name> | list ]"
+    _echo "usage: $(basename "${0}") [ <card_name> | list ]"
     exit 1
 }
 
-(( ${#} == 0 )) && _usage
+[ "${#}" -ne 1 ] && _usage
 
 _unmute_max_all() {
     for channel in $(amixer | grep -B1 "Capabilities:.*pvolume" | grep -oP "(?<=Simple mixer control ').+(?=')")
     do
-        echo "Unmuting ${channel} to 100%"
-        amixer set "${channel}" unmute &> /dev/null
-        amixer set "${channel}" 100% &> /dev/null
+        _echo "Unmuting ${channel} to 100%"
+        amixer set "${channel}" unmute > /dev/null 2>&1
+        amixer set "${channel}" 100% > /dev/null 2>&1
     done
 }
 
 _write_asoundrc() {
-    echo -e "defaults.ctl.card ${1}\ndefaults.pcm.card ${1}" > "${HOME}/.asoundrc"
+    printf 'defaults.ctl.card %s\ndefaults.pcm.card %s\n' "${1}" "${1}" > "${HOME}/.asoundrc"
 }
 
 _switch_card() {
-    local -r search="$(grep -iwH "^${1}$" /proc/asound/card*/id)"
-    if [[ -n "${search}" ]]
+    _search="$(grep -iwH "^${1}$" /proc/asound/card*/id)"
+    if [ -n "${_search}" ]
     then
-        local -r index="$(echo "${search%:*}" | grep -oP '(?<=/proc/asound/card)[0-9]+(?=/id)')"
-        _write_asoundrc "${index}"
-        echo "Switched to card ${search##*:} (${index})"
+        _index="$(_echo "${_search%:*}" | grep -oP '(?<=/proc/asound/card)[0-9]+(?=/id)')"
+        _write_asoundrc "${_index}"
+        _echo "Switched to card ${_search##*:} (${_index})"
     else
-        echo "Card ${1} not found, exiting."
+        _echo "Card ${1} not found, exiting."
         exit 1
     fi
 }
