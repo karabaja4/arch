4373b7349a20913121f655ce6c40b73325529334
karabaja4 <karabaja4@gmail.com>
2021-06-12 22:14:04 +0200

    a

--- nvidia/nvidia.sh
+++ nvidia/nvidia.sh
@@ -8,7 +8,8 @@ sudo modprobe nvidia_drm modeset=1
 export LD_LIBRARY_PATH=/usr/lib64/nvidia/:/usr/lib32/nvidia:/usr/lib:${LD_LIBRARY_PATH}
 
 _rootdir="$(dirname "$(readlink -f "${0}")")"
-sudo ln -sfv "${_rootdir}/nvidia-xorg.conf" "/etc/X11/nvidia-xorg.conf"
-sudo ln -sfv "${_rootdir}/nvidia-xorg.conf.d" "/etc/X11/nvidia-xorg.conf.d"
+
+sudo ln -sfvT "${_rootdir}/nvidia-xorg.conf" "/etc/X11/nvidia-xorg.conf"
+sudo ln -sfvT "${_rootdir}/nvidia-xorg.conf.d" "/etc/X11/nvidia-xorg.conf.d"
 
 exec xinit "${_rootdir}/xinitrc" -- :0 vt"$(fgconsole)" -nolisten tcp -br -config nvidia-xorg.conf -configdir nvidia-xorg.conf.d
