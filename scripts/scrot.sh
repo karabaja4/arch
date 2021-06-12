2c374d38a77afb16999cfaa64f0e9f2af8da12de
karabaja4 <karabaja4@gmail.com>
2021-06-13 01:28:50 +0200

    a

--- scripts/scrot.sh
+++ scripts/scrot.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/sh
+set -eu
 
-_shot() {
-    local -a _resolutions=()
-    mapfile -t _resolutions < <(xrandr | grep -o '[0-9]*x[0-9]*[+-][0-9]*[+-][0-9]*')
-    local -i idx=1
-    for res in "${_resolutions[@]}"
+_shot() (
+    _idx=1
+    xrandr | grep -o '[0-9]*x[0-9]*[+-][0-9]*[+-][0-9]*' | while read -r res
     do
-        echo "Screenshoting: ${res} (${idx})"
-        maim -u -g "${res}" > "/tmp/screenshots/$(date +%s%N)_${idx}.png"
-        (( idx++ ))
+        echo "Screenshoting: ${res} (${_idx})"
+        maim -u -g "${res}" > "/tmp/screenshots/$(date +%s%N)_${_idx}.png"
+        _idx=$(( _idx + 1 ))
     done
-}
+)
 
 mkdir -p /tmp/screenshots/
 _shot
