ee3311548cf275f4f3c54b725089f96632df8da8
karabaja4 <karabaja4@gmail.com>
2021-06-12 15:09:13 +0200

    a

--- scripts/dfree.sh
+++ scripts/dfree.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-set -uo pipefail
+#!/bin/sh
+set -u
 
-declare -r _path="/dev/disk/by-uuid/23147c37-efb6-4837-83aa-87808c70b87a"
+_path="/dev/disk/by-uuid/23147c37-efb6-4837-83aa-87808c70b87a"
 
 if [ -b "${_path}" ]
 then
 	df "${_path}" | tail -1 | awk '{print $2" "$4}'
 else
-	echo 0 0
+	printf '%s %s\n' 0 0
 fi
