bebac7922f963eba932dd2b81af924629e611ac0
karabaja4 <karabaja4@gmail.com>
2021-06-12 23:04:41 +0200

    a

--- nvidia/nvidia.sh
+++ nvidia/nvidia.sh
@@ -8,8 +8,20 @@ sudo modprobe nvidia_drm modeset=1
 export LD_LIBRARY_PATH=/usr/lib64/nvidia/:/usr/lib32/nvidia:/usr/lib:${LD_LIBRARY_PATH}
 
 _rootdir="$(dirname "$(readlink -f "${0}")")"
-
 sudo ln -sfvT "${_rootdir}/nvidia-xorg.conf" "/etc/X11/nvidia-xorg.conf"
 sudo ln -sfvT "${_rootdir}/nvidia-xorg.conf.d" "/etc/X11/nvidia-xorg.conf.d"
 
-exec xinit "${_rootdir}/xinitrc" -- :0 vt"$(fgconsole)" -nolisten tcp -br -config nvidia-xorg.conf -configdir nvidia-xorg.conf.d
+_sockdir="/tmp/.X11-unix"
+mkdir -pv "${_sockdir}"
+chmod 1777 "${_sockdir}"
+
+_get_screen() (
+    _index=0
+    while [ -e "${_sockdir}/X${_index}" ]
+    do
+        _index=$(( _index + 1 ))
+    done
+    printf '%s\n' "${_index}"
+)
+
+exec xinit "${_rootdir}/xinitrc" -- ":$(_get_screen)" "vt$(fgconsole)" -nolisten tcp -br -config nvidia-xorg.conf -configdir nvidia-xorg.conf.d
