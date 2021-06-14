a3ea373a63b8589eec9740c5475b33fca0204cca
karabaja4 <karabaja4@gmail.com>
2021-06-14 03:03:31 +0200

    a

--- scripts/scrot.sh
+++ scripts/scrot.sh
@@ -3,7 +3,7 @@ set -eu
 
 _shot() {
     _idx=1
-    xrandr | grep -o '[0-9]*x[0-9]*[+-][0-9]*[+-][0-9]*' | while read -r res
+    for res in $(xrandr | grep -o '[0-9]*x[0-9]*[+-][0-9]*[+-][0-9]*')
     do
         echo "Screenshoting: ${res} (${_idx})"
         maim -u -g "${res}" > "/tmp/screenshots/$(date +%s%N)_${_idx}.png"
