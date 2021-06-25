7403636e752b380ca3e65680a80ad7d25bc541f4
karabaja4 <karabaja4@gmail.com>
2021-06-26 00:20:12 +0200

    a

--- qtfm/term.sh
+++ qtfm/term.sh
@@ -10,7 +10,8 @@ _exec() {
     local -a _params
     mapfile -t _params <<< "${_temp}"
 
-    for i in "${!_params[@]}"; do 
+    for i in "${!_params[@]}"
+    do 
         printf '\033[32m%s\033[0m\n' "${i}: ${_params[${i}]}"
     done
 
