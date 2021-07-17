9a87a6f1c3325122fa0aaef07e01d205c10c71f0
karabaja4 <karabaja4@gmail.com>
2021-07-17 19:30:53 +0200

    a

--- scripts/dpms.sh
+++ scripts/dpms.sh
@@ -1,35 +1,10 @@
 #!/bin/sh
 set -eu
 
-_echo() {
-    printf '%s\n' "${1}"
-}
-
-_echo "$(basename "${0}") @ $(readlink /proc/$$/exe)"
-_current="$(xset -display :0.0 q | awk '/DPMS is/ { print $3 }')"
-_echo "DPMS: ${_current}"
-
-_enable() {
-    if [ "${_current}" = "Disabled" ]
-    then
-        xset -display :0.0 +dpms
-        _echo "DPMS: ${_current} -> Enabled"
-    fi
-}
-
-_disable() {
-    if [ "${_current}" = "Enabled" ]
-    then
-        xset -display :0.0 -dpms
-        _echo "DPMS: ${_current} -> Disabled"
-    fi
-}
-
-if grep -q "RUNNING" /proc/asound/card*/pcm*/sub*/status
+if ! grep -q "RUNNING" /proc/asound/card*/pcm*/sub*/status
 then
-    _echo "ALSA: Running"
-    _disable
-else
-    _echo "ALSA: Stopped"
-    _enable
+    if [ "$(DISPLAY=:0 xprintidle)" -gt 300000 ] # 5 min = 300000 ms
+    then
+        xset -display :0.0 dpms force off
+    fi
 fi
