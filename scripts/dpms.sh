3decc6847fed83f04ae8606e2ad09bd61b0d654e
karabaja4 <karabaja4@gmail.com>
2021-07-21 22:01:57 +0200

    a

--- scripts/dpms.sh
+++ scripts/dpms.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 
+xset -display :0.0 dpms 0 0 0
+
 if ! grep -q "RUNNING" /proc/asound/card*/pcm*/sub*/status
 then
     if [ "$(DISPLAY=:0 xprintidle)" -gt 300000 ] # 5 min = 300000 ms
