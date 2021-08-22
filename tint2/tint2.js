8c482f54edc141b8cef4376ebe725bd0c690b405
karabaja4 <karabaja4@gmail.com>
2021-08-23 01:34:43 +0200

    a

--- tint2/tint2.js
+++ tint2/tint2.js
@@ -107,7 +107,7 @@ const print = async () => {
 const conky = async () => {
   while (true) {
     try {
-      const content = await fs.promises.readFile('/tmp/conky-tint2.json', 'utf8');
+      const content = await fs.promises.readFile('/tmp/conky-tint2.fifo', 'utf8');
       if (content) {
         data.conky = JSON.parse(content.replaceAll(/[\t\n\r]/gm, ''));
       }
