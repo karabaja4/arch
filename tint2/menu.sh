8dce9a0888d109fc6efc74dbeb78c6eb437d71e2
karabaja4 <karabaja4@gmail.com>
2021-09-23 21:45:03 +0200

    a

--- tint2/menu.sh
+++ tint2/menu.sh
@@ -2,7 +2,8 @@
 
 case "${1}" in
 pd2)
-    sh -c "cd /home/igor/.wine/drive_c/d2/ProjectD2 && __GL_FSAA_MODE=11 wine Game.exe"
+    cd /home/igor/.wine/drive_c/d2/ProjectD2 || exit
+    __GL_FSAA_MODE=11 wine Game.exe
     ;;
 wow)
     wine /home/igor/.wine/drive_c/wow/WoW.exe
