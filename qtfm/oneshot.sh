864f259299aac02f8ab6c6164a0393c51cd287fc
karabaja4 <karabaja4@gmail.com>
2022-01-17 23:24:21 +0100

    a

--- qtfm/oneshot.sh
+++ qtfm/oneshot.sh
@@ -2,8 +2,11 @@
 set -euo pipefail
 
 case "${1}" in
-copypath)
+selectcopy)
     printf '%s\n' "${@:2}" > /tmp/qtfm.paths
+    ;;
+copypath)
+    echo -n "${2}" | xclip -i -selection clipboard
     ;;
 openterm)
     xfce4-terminal --working-directory="${PWD}"
