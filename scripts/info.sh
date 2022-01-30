965dcb120527cc5cc3e9b626c7bc7b3c32286345
karabaja4 <karabaja4@gmail.com>
2022-01-30 20:53:37 +0100

    a

--- scripts/info.sh
+++ scripts/info.sh
@@ -10,7 +10,7 @@ _mem_used_gb="$( printf '%s %s' "${_mem_total_kb}" "${_mem_available_kb}" | awk 
 _mem_total_gb="$( printf '%s' "${_mem_total_kb}" | awk '{ print $1 / 1048576 }' )"
 
 printf '\n'
-printf '  Arch %s\n\n' "$(sed 's/ (.*//' /proc/version)"
+printf '  Arch %s\n\n' "$(sed 's/ (.*//g; s/version/@/' /proc/version)"
 printf '  * CPU:      %s\n' "$(grep 'model name' /proc/cpuinfo | head -n1 | sed 's/model name\t: //')"
 printf '  * Memory:   %.2f GB / %.2f GB\n' "${_mem_used_gb}" "${_mem_total_gb}"
 printf '  * Shell:    %s\n' "$(readlink /proc/$PPID/exe | sed 's/\/usr\/bin\///')"
@@ -22,7 +22,7 @@ printf '  '
 _i=0
 while [ ${_i} -le 15 ]
 do
-    if [ ${_i} = 8 ]
+    if [ ${_i} -eq 8 ]
     then
         printf '\n  '
     fi
