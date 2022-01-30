7d87551733d343e7f8af823c7d20aa476daab509
karabaja4 <karabaja4@gmail.com>
2022-01-30 21:16:22 +0100

    a

--- scripts/fixres.sh
+++ scripts/fixres.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-# left
-xrandr --output HDMI-1-1 --mode 1920x1200 --primary
+_screen_middle="HDMI-1-1"
+_screen_right="DP-1-1"
+_screen_left="eDP-1-1"
+
+_res_middle="1920x1200"
+_res_right="1920x1200"
+_res_left="1920x1080"
+
+# middle
+xrandr --output "${_screen_middle}" --mode "${_res_middle}" --primary
 
 # right
-xrandr --addmode DP-1-1 1920x1200
-xrandr --output DP-1-1 --mode 1920x1200 --right-of HDMI-1-1
+xrandr --addmode "${_screen_right}" "${_res_right}"
+xrandr --output "${_screen_right}" --mode "${_res_right}" --right-of "${_screen_middle}"
 
-# laptop
-xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-1-1
+# left
+xrandr --output "${_screen_left}" --mode "${_res_left}" --left-of "${_screen_middle}"
