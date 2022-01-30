29b824359f50d3b8ac4725731966e793ce997322
karabaja4 <karabaja4@gmail.com>
2022-01-30 13:46:57 +0100

    a

---
+++ scripts/linode.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+_mount() {
+    _user="$(jq -crM '.linode.samba.username' "${HOME}/arch/secret.json")"
+    _pass="$(jq -crM '.linode.samba.password' "${HOME}/arch/secret.json")"
+    doas mount -t cifs -o username="${_user}",password="${_pass}",uid="$(id -u)",gid="$(id -g)" "${@}"
+}
+
+_main() {
+    mkdir -p "${HOME}/_public"
+    mkdir -p "${HOME}/_private"
+    _mount "//avacyn.aerium.hr/public" "${HOME}/_public"
+    _mount "//avacyn.aerium.hr/private" "${HOME}/_private"
+}
+
+_main
