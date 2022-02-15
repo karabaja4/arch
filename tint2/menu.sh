2fd56f038fabe8eda919d91e1f9e30efe3cb2b9f
karabaja4 <karabaja4@gmail.com>
2022-02-15 22:37:36 +0100

    a

--- tint2/menu.sh
+++ tint2/menu.sh
@@ -49,8 +49,8 @@ slack)
     _run slack --disable-smooth-scrolling --start-maximized
     ;;
 teams)
-    #_run chromium --app=https://teams.microsoft.com
-    _run /home/igor/arch/misc/teams/teams2
+    _run chromium --app=https://teams.microsoft.com
+    #_run /home/igor/arch/misc/teams/teams2
     ;;
 discord)
     _run chromium --app=https://discord.com/app
@@ -59,8 +59,8 @@ skype)
     _run chromium --app=https://web.skype.com
     ;;
 paint)
-    #_run chromium --app=https://jspaint.app
-    _run falkon -e https://jspaint.app
+    _run chromium --app=https://jspaint.app
+    #_run falkon -e https://jspaint.app
     ;;
 vlc)
     _run vlc
