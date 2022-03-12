e8b4aa255fb15a4ccc1ce2342db9ace6969c5979
karabaja4 <karabaja4@gmail.com>
2022-03-12 20:00:39 +0100

    a

--- scripts/dpms.sh
+++ scripts/dpms.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 set -eu
 
-xset -display :1 dpms 0 0 0
+export XAUTHORITY="${HOME}/.local/share/sx/xauthority"
+export DISPLAY=":1"
+
+xset dpms 0 0 0
 
 if ! grep -q "RUNNING" /proc/asound/card*/pcm*/sub*/status
 then
-    if [ "$(DISPLAY=:1 xprintidle)" -gt 300000 ] # 5 min = 300000 ms
+    if [ "$(xprintidle)" -gt 300000 ] # 5 min = 300000 ms
     then
-        xset -display :1 dpms force off
+        xset dpms force off
     fi
 fi
