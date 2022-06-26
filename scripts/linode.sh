4b88078753a50def932c440ef7ba918e95131c61
karabaja4 <karabaja4@gmail.com>
2022-06-26 18:39:48 +0200

    a

--- scripts/linode.sh
+++ scripts/linode.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+_uid="$(id -u)"
+_gid="$(id -g)"
+
 _mount() {
-    _user="$(jq -crM '.linode.samba.username' "${HOME}/arch/secret.json")"
-    _pass="$(jq -crM '.linode.samba.password' "${HOME}/arch/secret.json")"
-    doas mount -t cifs -o username="${_user}",password="${_pass}",uid="$(id -u)",gid="$(id -g)",dir_mode=0755,file_mode=0644,port=44555 "${@}"
+    _user="$(jq -crM '.linode.username' "${HOME}/arch/secret.json")"
+    _pass="$(jq -crM '.linode.password' "${HOME}/arch/secret.json")"
+    doas mount -t cifs -o username="${_user}",password="${_pass}",uid="${_uid}",gid="${_gid}",dir_mode=0755,file_mode=0644,port=44555 "${@}"
+}
+
+_azure() {
+    _user="$(jq -crM '.azure.username' "${HOME}/arch/secret.json")"
+    _pass="$(jq -crM '.azure.password' "${HOME}/arch/secret.json")"
+    doas mount -t cifs -o vers=3.0,username="${_user}",password="${_pass}",uid="${_uid}",gid="${_gid}",dir_mode=0755,file_mode=0644,serverino,nosharesock,actimeo=30 "${@}"
 }
 
 mkdir -p "${HOME}/_public"
 mkdir -p "${HOME}/_private"
+mkdir -p "${HOME}/_azure"
+
 _mount "//avacyn.aerium.hr/public" "${HOME}/_public"
 _mount "//avacyn.aerium.hr/private" "${HOME}/_private"
+_azure "//linode.file.core.windows.net/azure1" "${HOME}/_azure"
