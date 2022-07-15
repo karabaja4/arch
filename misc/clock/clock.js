0247b43c4a23bd03fc6647f5c3d2eb9f5a4002e2
karabaja4 <karabaja4@gmail.com>
2022-07-15 15:28:47 +0200

    a

--- misc/clock/clock.js
+++ misc/clock/clock.js
@@ -23,13 +23,24 @@ const main = async () => {
     const date = new Date(localDate());
     const h = date.getHours();
     const m = date.getMinutes().toString().padStart(2, '0');
-    const s = date.getSeconds().toString().padStart(2, '0');
+    const sec = date.getSeconds();
+    let s = "";
+    let numeric = false;
+    if (sec === 30) s = "Have";
+    else if (sec === 31) s = "a";
+    else if (sec === 32) s = "nice";
+    else if (sec === 33) s = "day";
+    else if (sec === 34) s = ":)"
+    else {
+      s = sec.toString().padStart(2, '0');
+      numeric = true;
+    }
     const result = {
       text: `${h}:${m}:${s}`,
       color: colors[idx++ % colors.length],
       size: 200,
       interval: interval,
-      digital: true
+      digital: numeric
     };
     await fs.promises.writeFile('/home/igor/_static/ad307b2c60c32dc4.json', JSON.stringify(result));
     await timers.setTimeout(interval);
