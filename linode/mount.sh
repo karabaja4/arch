5c69d5299efc653534b8bb93d4288b0824166af1
karabaja4 <karabaja4@gmail.com>
2022-07-16 14:57:05 +0200

    a

---
+++ linode/mount.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+_user="igor"
+_uid="$(id -u "${_user}")"
+_gid="$(id -g "${_user}")"
+
+_mount() {
+    mount -t cifs -o vers=3.0,credentials=/etc/smbcredentials/linode.cred,uid="${_uid}",gid="${_gid}",dir_mode=0755,file_mode=0644,serverino,nosharesock,actimeo=30 "${@}"
+}
+
+_mount "//linode.file.core.windows.net/public1" "/home/${_user}/_public"
+_mount "//linode.file.core.windows.net/private1" "/home/${_user}/_private"
