a9546d764e6946a1d212c71991202f1c4bd552bc
karabaja4 <karabaja4@gmail.com>
2022-07-23 11:10:23 +0200

    a

--- clock/clock.js
+++ clock/clock.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const timers = require('timers/promises');
+const express = require('express');
+const app = express();
 
 const colors = [
   "#0000ff",
@@ -33,32 +33,35 @@ const greetings = (hour) => {
 };
 
 const interval = 1000;
-let idx = 0;
 
 const localDate = () => {
   return new Date().toLocaleString('en-US', { timeZone: 'Europe/Zagreb' });
 };
 
-const main = async () => {
-  while (true) {
-    const date = new Date(localDate());
-    const h = date.getHours();
-    const m = date.getMinutes().toString().padStart(2, '0');
-    const sec = date.getSeconds();
+const port = 42811;
 
-    const words = greetings(h).split(' ');
-    const word = words[sec - 30];
+app.get('/', async (req, res) => {
 
-    const result = {
-      text: word || `${h}:${m}:${sec.toString().padStart(2, '0')}`,
-      color: colors[idx++ % colors.length],
-      size: word ? 180 : 200,
-      interval: interval,
-      digital: !word
-    };
-    await fs.promises.writeFile('/home/igor/_static/ad307b2c60c32dc4.json', JSON.stringify(result));
-    await timers.setTimeout(interval);
-  }
-};
+  const date = new Date(localDate());
+  const h = date.getHours();
+  const m = date.getMinutes().toString().padStart(2, '0');
+  const sec = date.getSeconds();
+  const ts = Math.trunc(date.getTime() / 1000);
 
-main();
+  const words = greetings(h).split(' ');
+  const word = words[sec - 30];
+
+  const result = {
+    text: word || `${h}:${m}:${sec.toString().padStart(2, '0')}`,
+    color: colors[ts % colors.length],
+    size: word ? 180 : 200,
+    interval: interval,
+    digital: !word
+  };
+
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+});
