93cc751490822a7cb5bbb126b82537dd9987cd67
karabaja4 <karabaja4@gmail.com>
2022-07-24 03:19:37 +0200

    a

--- feh/feh.sh
+++ feh/feh.sh
@@ -3,10 +3,10 @@
 exec feh \
 --draw-actions \
 --draw-filename \
---fontpath "/usr/share/fonts/TTF" \
---font "Roboto-Bold/10" \
+--fontpath '/usr/share/fonts/TTF' \
+--font 'Roboto-Bold/10' \
 --action1 ';[Copy to clipboard]xclip -selection clipboard -t image/png -i %F' \
---action2 ';[Copy file to /home/igor]cp %F /home/igor/%N' \
+--action2 ";[Copy file to ${HOME}]cp %F ${HOME}/%N" \
 --action3 ';[Set as wallpaper]xwallpaper --stretch %F' \
 --no-menus \
 --scale-down \
