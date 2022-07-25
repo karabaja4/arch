139bf49777b213047e87698fdfb10bbb1692ba10
karabaja4 <karabaja4@gmail.com>
2022-07-25 20:25:02 +0200

    a

--- clock/clock.js
+++ clock/clock.js
@@ -7,23 +7,22 @@ app.get('/tick/:id', async (req, res) => {
 
   console.log(`tick @ ${req.params.id}`);
 
-  // // GT-I9300
-  // if (req.params.id === 'ad307b2c60c32dc4') {
-  //   res.send(getClock());
-  //   return;
-  // }
-  // // android studio emulator
-  // else if (req.params.id === 'b4041919f2a050e7') {
-  //   res.send(getHello());
-  //   return;
-  // }
-  // // btc
-  // else {
-  //   res.send(getPrice());
-  //   return;
-  // }
-
-  res.send(getHello());
+  // GT-I9300
+  if (req.params.id === 'ad307b2c60c32dc4') {
+    res.send(getClock());
+    return;
+  }
+  // android studio emulator
+  else if (req.params.id === 'b4041919f2a050e7') {
+    res.send(getHello());
+    return;
+  }
+  // btc
+  else {
+    res.send(getPrice());
+    return;
+  }
+  
 });
 
 const getHello = () => {
