49b7e7494e65e1cedfe34818a24b11decc0605fd
karabaja4 <karabaja4@gmail.com>
2022-07-26 23:43:48 +0200

    a

--- clock/clock.js
+++ clock/clock.js
@@ -77,7 +77,7 @@ const getClock = (size) => {
     text: word || `${h}:${m}:${sec.toString().padStart(2, '0')}`,
     color: colors[ts % colors.length],
     size: word ? (size - 20) : size,
-    interval: 300,
+    interval: 1000,
     digital: !word
   };
   return result;
