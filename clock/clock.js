39fee04254f2f8bb77e122798e463dd4c86ba8b8
karabaja4 <karabaja4@gmail.com>
2022-07-26 02:11:45 +0200

    a

--- clock/clock.js
+++ clock/clock.js
@@ -33,7 +33,7 @@ app.get('/tick/:id', async (req, res) => {
   
 });
 
-const getAmount = () => {
+const getAmount = async () => {
   const am = await axios.get('http://127.0.0.1:32781/amount');
   return am.data;
 }
