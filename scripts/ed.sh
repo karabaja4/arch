4f56e24ace7fa19807e41fe0d4252c4949933591
karabaja4 <karabaja4@gmail.com>
2022-08-09 02:10:15 +0200

    a

--- scripts/ed.sh
+++ scripts/ed.sh
@@ -2,8 +2,9 @@
 
 # ln -sf /home/igor/arch/scripts/ed.sh /usr/bin/ed
 
-if [ "${TERM}" = "linux" ]
+if [ "${TERM}" = "linux" ] || [ "$(id -u)" -eq 0 ]
 then
+    # no GUI or root
     nano "${1}"
 else
     qtextpad --syntax "INI Files" "${1}" > /dev/null 2>&1
