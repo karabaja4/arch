8c2e09b0209a6491a918e131f2cdebb43d5a6595
karabaja4 <karabaja4@gmail.com>
2022-08-28 16:01:54 +0200

    a

--- lf/lf.sh
+++ lf/lf.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-_path="$(realpath "$(printf '%s' "${1}" | sed 's;file://;;g')")"
-xfce4-terminal --title "lf" --command "lf ${_path}"
+_p1="$(printf '%s' "${1}" | sed 's;file://;;g')"
+_p2="$(realpath "${_p1}")"
+
+xfce4-terminal --title "lf" --command "lf '${_p2}'"
