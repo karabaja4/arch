c7a8e1ccbfd8d35e17d2b33fe44beaf18dc9ca24
karabaja4 <karabaja4@gmail.com>
2022-09-13 02:10:18 +0200

    a

--- scripts/info.sh
+++ scripts/info.sh
@@ -11,11 +11,11 @@ _mem_total_gb="$( printf '%s' "${_mem_total_kb}" | awk '{ print $1 / 1048576 }' 
 
 printf '\n'
 printf '  %s @ %s\n\n' "$(cut -d '"' -f2 /etc/os-release | head -n1)" "$(cut -d' ' -f3 /proc/version)"
-printf '  ● CPU:      %s\n' "$(grep 'model name' /proc/cpuinfo | head -n1 | sed 's/model name\t: //')"
-printf '  ● Memory:   %.2f GB / %.2f GB\n' "${_mem_used_gb}" "${_mem_total_gb}"
-printf '  ● Shell:    %s\n' "$(readlink /proc/$PPID/exe | sed 's/\/usr\/bin\///')"
-printf '  ● Uptime:   %s\n' "$(uptime -p | cut -c 4-)"
-printf '  ● Packages: %s (%s AUR)\n' "$(pacman -Qq | wc -l)" "$(pacman -Qm | wc -l)"
+printf '  * CPU:      %s\n' "$(grep 'model name' /proc/cpuinfo | head -n1 | sed 's/model name\t: //')"
+printf '  * Memory:   %.2f GB / %.2f GB\n' "${_mem_used_gb}" "${_mem_total_gb}"
+printf '  * Shell:    %s\n' "$(readlink /proc/$PPID/exe | sed 's/\/usr\/bin\///')"
+printf '  * Uptime:   %s\n' "$(uptime -p | cut -c 4-)"
+printf '  * Packages: %s (%s AUR)\n' "$(pacman -Qq | wc -l)" "$(pacman -Qm | wc -l)"
 printf '\n'
 
 printf '  '
@@ -26,7 +26,7 @@ _print_color_row() {
     while [ ${_i} -lt 8 ]
     do
         _code=$((_base+_i))
-        printf '\033[%sm⏺\033[0m  ' "${_code}"
+        printf '\033[%sm████\033[0m' "${_code}"
         _i=$((_i+1))
     done
 }
