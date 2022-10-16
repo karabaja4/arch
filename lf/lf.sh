a2a94ee8c4024a6ef015b4fa958ff24f297466f0
karabaja4 <karabaja4@gmail.com>
2022-10-16 20:19:12 +0200

    a

--- lf/lf.sh
+++ lf/lf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-_p1="$(printf '%s' "${1}" | sed 's;file://;;g')"
+_p1="$(printf '%s' "${1}" | sed 's;file://;;g' | sed 's;%20; ;g')"
 _p2="$(realpath "${_p1}")"
 
-xfce4-terminal --title "lf" --command "lf '${_p2}'"
+xfce4-terminal --title "${USER}@$(hostname):~" --execute bash -c "lf '${_p2}';bash"
