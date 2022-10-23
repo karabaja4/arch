e7a6ba964cd97a0430989e448481a96498fd9e71
karabaja4 <karabaja4@gmail.com>
2022-10-24 00:44:34 +0200

    a

--- scripts/network.sh
+++ scripts/network.sh
@@ -2,5 +2,5 @@
 
 _interface="enp2s0"
 
-ip link set "${_interface}" up
+/usr/bin/ip link set "${_interface}" up
 exec /usr/bin/busybox udhcpc -nRfv -s "/home/igor/arch/udhcpc/default.script" -i "${_interface}"
