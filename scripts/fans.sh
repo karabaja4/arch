b2968aeb7f180854af4a24ce99fd86ee6fb0f33b
karabaja4 <karabaja4@gmail.com>
2023-03-03 03:17:25 +0100

    a

--- scripts/fans.sh
+++ scripts/fans.sh
@@ -7,9 +7,13 @@ _not_root() {
 
 [ "$(id -u)" -ne 0 ] && _not_root
 
+### settings ###
+
 _temp1=40
 _temp2=80
 _interval=10
+
+################
 
 _t1="$(( _temp1 * 1000 ))"
 _t2="$(( _temp2 * 1000 ))"
@@ -22,14 +26,21 @@ _output_range="$(( _v2 - _v1 ))"
 
 while true
 do
-    _input="$(cat /sys/devices/platform/coretemp.0/hwmon/hwmon5/temp1_input)"
-    _value="$(( (((_input - _t1) * _output_range) / _input_range) + _v1 ))"
+    _input1="$(cat /sys/devices/platform/coretemp.0/hwmon/hwmon5/temp1_input)"
+    _input2="$(cat /sys/devices/virtual/thermal/thermal_zone1/temp)"
+
+    if [ "${_input2}" -gt "${_input1}" ]
+    then
+        _input1="${_input2}"
+    fi
+
+    _value="$(( (((_input1 - _t1) * _output_range) / _input_range) + _v1 ))"
     if [ "${_value}" -gt 255 ]
     then
         _value=255;
     fi
 
-    printf '%s°C -> %s\n' "$(( _input / 1000 ))" "${_value}"
+    printf '%s°C -> %s\n' "$(( _input1 / 1000 ))" "${_value}"
     printf '%s' "${_value}" > /sys/devices/platform/asus-nb-wmi/hwmon/hwmon4/pwm1
     sleep "${_interval}"
 done
