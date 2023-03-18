d1fc1c95d18c9e47de98bcd897699a2821605046
karabaja4 <karabaja4@gmail.com>
2023-03-18 03:50:27 +0100

    a

---
+++ misc/ads.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+_file='/opt/azuredatastudio/resources/app/out/vs/workbench/workbench.desktop.main'
+_js="${_file}.js"
+_css="${_file}.css"
+
+_success() {
+    printf '\033[32m%s\033[0m\n' "${1} was patched successfully"
+}
+
+_fail() {
+    printf '\033[31m%s\033[0m\n' "${1} was not changed"
+}
+
+_sum1="$(md5sum "${_js}")"
+# patch 1
+sed -i 's/this._connectionStore.saveProfile(\$,void 0,j);/this._connectionStore.saveProfile(\$,true,j);/g' "${_js}"
+# -------
+_sum2="$(md5sum "${_js}")"
+
+if [ "${_sum1}" != "${_sum2}" ]
+then
+    _success "${_js}"
+else
+    _fail "${_js}"
+fi
+
+_sum1="$(md5sum "${_css}")"
+# patch 2
+printf '\n%s\n%s' '.monaco-workbench > .notifications-toasts.visible { display:none; }' '.notifications-toasts { display:none; }' >> "${_css}"
+# -------
+_sum2="$(md5sum "${_css}")"
+
+if [ "${_sum1}" != "${_sum2}" ]
+then
+    _success "${_css}"
+else
+    _fail "${_css}"
+fi
