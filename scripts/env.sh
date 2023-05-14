191d25c3441b9aa7afab95c450e79da7098d88fa
karabaja4 <karabaja4@gmail.com>
2023-05-15 01:23:41 +0200

    a

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,9 +4,10 @@
 export QT_QPA_PLATFORMTHEME='qt5ct'
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export NO_AT_BRIDGE=1
-export EDITOR=nano
+export EDITOR='nano'
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
 export LESSHISTFILE=/dev/null
+export BROWSER='firefox-socket'
 
 # vnc does not export $SHELL
 export SHELL='/bin/bash'
