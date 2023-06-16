aa0b1b46d9d1e08e8cf42d3dc2552e53065f866a
karabaja4 <karabaja4@gmail.com>
2023-06-16 18:01:57 +0200

    a

--- feh/feh.sh
+++ feh/feh.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
 exec feh \
---draw-actions \
 --draw-filename \
 --fontpath '/usr/share/fonts/TTF' \
 --font 'Roboto-Bold/8' \
---action1 ';[to clipboard]xclip -selection clipboard -t image/png -i %F' \
---action2 ";[to ${HOME}]cp %F ${HOME}/%N" \
---action3 ';[as wallpaper]xwallpaper --stretch %F' \
 --no-menus \
 --scale-down \
 --geometry 1280x720 \
