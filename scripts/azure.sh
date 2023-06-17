856da0eeba7891f8f549ef7bb4e8602edbec1777
karabaja4 <karabaja4@gmail.com>
2023-06-18 01:49:43 +0200

    a

--- scripts/azure.sh
+++ scripts/azure.sh
@@ -7,7 +7,7 @@ _gid="$(id -g)"
 _azure() {
     _user="$(jq -crM '.azure.username' "${HOME}/arch/secret.json")"
     _pass="$(jq -crM '.azure.password' "${HOME}/arch/secret.json")"
-    doas mount -t cifs -o vers=3.0,username="${_user}",password="${_pass}",uid="${_uid}",gid="${_gid}",dir_mode=0755,file_mode=0644,serverino,nosharesock,actimeo=30 "${@}"
+    doas mount -t cifs -o username="${_user}",password="${_pass}",uid="${_uid}",gid="${_gid}",dir_mode=0755,file_mode=0644,serverino,nosharesock,actimeo=30 "${@}"
 }
 
 mkdir -p "${HOME}/_public"
