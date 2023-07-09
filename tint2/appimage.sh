e5b36c49cd84dd32296f9020622c8e8b2500bfc1
karabaja4 <karabaja4@gmail.com>
2023-07-09 02:52:08 +0200

    a

--- tint2/appimage.sh
+++ tint2/appimage.sh
@@ -7,19 +7,32 @@ _echo() {
     printf '%s\n' "${1}"
 }
 
+_latest() {
+    find "$(dirname "${1}")" -name "$(basename "${1}")" -printf "%Ts/%p\n" | sort -nr | cut -d'/' -f2- | head -n1
+}
+
+_exec() {
+    if [ -z "${1}" ]
+    then
+        _echo "Appimage not found."
+        exit 1
+    fi
+    exec "${@}" 
+}
+
 _filename="$(basename "${0}")"
 _scriptpath="$(readlink -f "${0}")"
 _scriptname="$(basename "${_scriptpath}")"
 
 case "${_filename}" in
 onlyoffice)
-    exec /opt/appimage/DesktopEditors-x86_64.AppImage "${@}"
+    _exec /opt/appimage/DesktopEditors-x86_64.AppImage "${@}"
     ;;
 krita)
-    exec /opt/appimage/krita-*-x86_64.appimage "${@}"
+    _exec "$(_latest /opt/appimage/krita-*-x86_64.appimage)" "${@}"
     ;;
 inkscape)
-    exec /opt/appimage/Inkscape-*-x86_64.AppImage "${@}"
+    _exec "$(_latest /opt/appimage/Inkscape-*-x86_64.AppImage)" "${@}"
     ;;
 "${_scriptname}")
     _echo "Symlink the script to the app, e.g. ln -s ${_scriptpath} /usr/bin/onlyoffice"
