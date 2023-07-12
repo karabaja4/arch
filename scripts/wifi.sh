398171c7e9c492a9399c5ffb2c442769f79fe8f2
karabaja4 <karabaja4@gmail.com>
2023-07-13 01:56:23 +0200

    a

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -3,14 +3,22 @@ set -eu
 IFS='
 '
 
-_interface="wlp0s20u2u2u4"
+_usage() {
+    printf '%s\n%s\n' \
+        "Connect to a WiFi network." \
+        "Usage: $(basename "${0}") [interface (omit to autodetect)]"
+    exit 0
+}
+
+_arg1="${1-}"
+case "${_arg1}" in
+--help|-h)
+    _usage
+    ;;
+esac
 
 _echo() {
     printf '%s\n' "${1}"
-}
-
-_prompt() {
-    printf '%s' "${1}"
 }
 
 _not_root() {
@@ -20,16 +28,46 @@ _not_root() {
 
 [ "$(id -u)" -ne 0 ] && _not_root
 
-_invalid_input() {
-    _echo "Invalid input"
+# resolve interface
+_interface="$(printf '%s\n' /sys/class/net/*/wireless | cut -d/ -f5 | grep -v -F '*' | cat)"
+if [ -z "${_interface}" ]
+then
+    _echo "No wireless interfaces found."
     exit 2
-}
+fi
+
+if [ -z "${_arg1}" ]
+then
+    # no argument given, autodetect interface
+    _count="$(_echo "${_interface}" | wc -l)"
+    if [ "${_count}" -ne 1 ]
+    then
+        printf '%s\n%s\n%s\n' \
+            "More than one interface found:" \
+            "${_interface}" \
+            "Please specify an interface as an argument."
+        exit 3
+    else
+        _echo "Detected interface ${_interface}"
+    fi
+else
+    # interface provided by user on arg1
+    _match="$(_echo "${_interface}" | grep -F "${_arg1}" | cat)"
+    if [ -z "${_match}" ]
+    then
+        _echo "Interface ${_arg1} not found."
+        exit 4
+    else
+         _interface="${_match}"
+        _echo "Using interface ${_interface}"
+    fi
+fi
 
 ip link set "${_interface}" up
 
 # scan for networks and present a choice
 _echo "Scanning for networks..."
-_essids="$(iwlist "${_interface}" scan | grep 'ESSID' | cut -d'"' -f2)"
+_essids="$(iwlist "${_interface}" scan | grep -F 'ESSID' | cut -d'"' -f2)"
 _i=1
 for _essid in ${_essids}
 do
@@ -37,13 +75,29 @@ do
     _i=$((_i+1))
 done
 
+_prompt() {
+    printf '%s' "${1}"
+}
+
+_invalid_input() {
+    _echo "Invalid input."
+    exit 5
+}
+
 # read a choice and save
 _prompt "Select network: "
 read -r _idx
-_selected_essid="$(_echo "${_essids}" | sed -n "${_idx}p")"
-[ -z "${_selected_essid}" ] && _invalid_input
-_md5="$(_echo "${_selected_essid}" | md5sum | cut -d' ' -f1)"
-_echo "Selected: ${_selected_essid} (md5: ${_md5})"
+
+# check if there are non-numbers in input
+_idx_validate="$(_echo "${_idx}" | tr -d '0-9')"
+[ -n "${_idx_validate}" ] && _invalid_input
+
+# check the number is in range
+_ssid="$(_echo "${_essids}" | sed -n "${_idx}p")"
+[ -z "${_ssid}" ] && _invalid_input
+
+_md5="$(_echo "${_ssid}" | md5sum | cut -d' ' -f1)"
+_echo "Selected: ${_ssid} (md5: ${_md5})"
 
 # config paths
 _config_dir="/root/.config/wifi"
@@ -54,23 +108,23 @@ _config="${_config_dir}/${_md5}.conf"
 if [ ! -f "${_config}" ]
 then
     _prompt "Enter password: "
-    read -r _password
+    read -r _psk
     _echo "Saving config ${_config}"
     printf '%s\n%s\n%s\n%s\n%s\n' \
         "ctrl_interface=/run/wpa_supplicant" \
         "network={" \
-        "    ssid=\"${_selected_essid}\"" \
-        "    psk=\"${_password}\"" \
+        "    ssid=\"${_ssid}\"" \
+        "    psk=\"${_psk}\"" \
         "}" > "${_config}"
 else
     _echo "Using config ${_config}"
 fi
 
 # run with setsid to prevent terminal sending sigint to children
-setsid wpa_supplicant -c "${_config}" -i "${_interface}" &
+setsid /usr/bin/wpa_supplicant -c "${_config}" -i "${_interface}" &
 _pid1="${!}"
 
-setsid dhcpcd -4 -B "${_interface}" &
+setsid /usr/bin/dhcpcd -4 -B "${_interface}" &
 _pid2="${!}"
 
 # kill all on exit
