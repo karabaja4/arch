e0faa471169e35cd973c9b287d3507344416087e
karabaja4 <karabaja4@gmail.com>
2023-07-13 22:26:19 +0200

    a

--- scripts/azure.sh
+++ scripts/azure.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 set -eu
 
-_uid="$(id -u)"
-_gid="$(id -g)"
-_secret="${HOME}/arch/secret.json"
+_user="$(id -un 1000)"
+_home="/home/${_user}"
+
+_uid="$(id -u "${_user}")"
+_secret="${_home}/arch/secret.json"
 
 _azure() {
-    _user="$(jq -crM '.azure.username' "${_secret}")"
-    _pass="$(jq -crM '.azure.password' "${_secret}")"
-    doas mount -t cifs -o username="${_user}",password="${_pass}",uid="${_uid}",gid="${_gid}",dir_mode=0755,file_mode=0644,serverino,nosharesock,actimeo=30 "${@}"
+    _username="$(jq -crM '.azure.username' "${_secret}")"
+    _password="$(jq -crM '.azure.password' "${_secret}")"
+    mount -t cifs -o username="${_username}",password="${_password}",uid="${_uid}",dir_mode=0755,file_mode=0644,serverino,nosharesock,actimeo=30 "${@}"
 }
 
-mkdir -p "${HOME}/_public"
-mkdir -p "${HOME}/_private"
+_public="${_home}/_public"
+_private="${_home}/_private"
 
-_azure "//linode.file.core.windows.net/public1" "${HOME}/_public"
-_azure "//linode.file.core.windows.net/private1" "${HOME}/_private"
+mkdir -p "${_public}"
+mkdir -p "${_private}"
+
+_azure "//linode.file.core.windows.net/public1" "${_public}"
+_azure "//linode.file.core.windows.net/private1" "${_private}"
