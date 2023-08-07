f8393cff4aa98932a511115818780469d738213e
karabaja4 <karabaja4@gmail.com>
2023-08-08 00:55:24 +0200

    a

--- scripts/fans.sh
+++ scripts/fans.sh
@@ -22,10 +22,13 @@ _output_range="$(( _v2 - _v1 ))"
 _nvtype="$(grep -l TMEM /sys/devices/virtual/thermal/thermal_zone*/type)"
 _nvpath="${_nvtype%/*}/temp"
 
+_cpupath="/sys/devices/platform/coretemp.0/hwmon/hwmon5/temp1_input"
+_fanctrl="/sys/devices/platform/asus-nb-wmi/hwmon/hwmon4/pwm1"
+
 while true
 do
     # cpu temp
-    _input="$(<"/sys/devices/platform/coretemp.0/hwmon/hwmon5/temp1_input")"
+    _input="$(<"${_cpupath}")"
 
     # if nvidia is hotter, use that temp
     _nvtemp="$(<"${_nvpath}")"
@@ -41,6 +44,6 @@ do
     [ "${_value}" -gt "${_v2}" ] && _value="${_v2}"
 
     _echo "$(( _input / 1000 ))°C -> ${_value}"
-    _echo "${_value}" > /sys/devices/platform/asus-nb-wmi/hwmon/hwmon4/pwm1
+    _echo "${_value}" > "${_fanctrl}"
     sleep "${_interval}"
 done
