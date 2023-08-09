3f2edd61c61412f4ab6eb7693cca99d577692dac
karabaja4 <karabaja4@gmail.com>
2023-08-09 03:13:05 +0200

    a

--- scripts/ed.sh
+++ scripts/ed.sh
@@ -3,10 +3,14 @@ set -eu
 
 # ln -sf /home/igor/arch/scripts/ed.sh /usr/bin/ed
 
+_run() {
+    ( "${@}" & ) > /dev/null 2>&1
+}
+
 if [ "${TERM}" = "linux" ] || [ "$(id -u)" -eq 0 ]
 then
     # no GUI or root
     nano "${1-}"
 else
-    qtextpad "${1-}" > /dev/null 2>&1
+    _run qtextpad "${1-}"
 fi
