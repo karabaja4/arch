0e207f0151bac32e22aab1e00864173348d955c8
karabaja4 <karabaja4@gmail.com>
2023-09-02 22:39:47 +0200

    a

--- scripts/azure.sh
+++ scripts/azure.sh
@@ -18,5 +18,8 @@ _private="${_home}/_private"
 mkdir -p "${_public}"
 mkdir -p "${_private}"
 
-_azure "//linode.file.core.windows.net/public1" "${_public}"
-_azure "//linode.file.core.windows.net/private1" "${_private}"
+_azure "//share.radiance.hr/public1" "${_public}"
+_azure "//share.radiance.hr/private1" "${_private}"
+
+#_azure "//linode.file.core.windows.net/public1" "${_public}"
+#_azure "//linode.file.core.windows.net/private1" "${_private}"
