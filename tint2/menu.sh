27c9a9634abd860293dfce922e316a22d727f4b0
karabaja4 <karabaja4@gmail.com>
2023-10-15 17:22:33 +0200

    a

--- tint2/menu.sh
+++ tint2/menu.sh
@@ -73,7 +73,7 @@ flameshot)
 paint)
     #rm -f "/home/igor/.config/chromium/Default/Local Storage/leveldb/"*
     #_run chromium --start-maximized --disable-smooth-scrolling --disable-audio-output --app=https://jspaint.app
-    wine /home/igor/arch/mspaint/vista/mspaint.exe
+    wine64 /home/igor/arch/mspaint/xp64/mspaint.exe
     ;;
 onlyoffice)
     _run onlyoffice-desktopeditors
