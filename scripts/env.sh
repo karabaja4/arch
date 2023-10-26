c47115d2da1df6d71b5798904c088d1742bd72c7
karabaja4 <karabaja4@gmail.com>
2023-10-26 02:44:54 +0200

    a

--- scripts/env.sh
+++ scripts/env.sh
@@ -6,6 +6,7 @@ export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export NO_AT_BRIDGE=1
 export EDITOR='nano'
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
+export DOTNET_GENERATE_ASPNET_CERTIFICATE=false
 export LESSHISTFILE=/dev/null
 export BROWSER='firefox-socket'
 export HISTFILESIZE=2000
