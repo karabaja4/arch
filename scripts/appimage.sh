a292b509cc54f61b0cb22c54c2add7cbf65a6cf5
karabaja4 <karabaja4@gmail.com>
2023-12-03 04:13:27 +0100

    a

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -10,13 +10,13 @@ onlyoffice-desktopeditors)
     exec "/opt/appimage/DesktopEditors-x86_64.AppImage" "${@}"
     ;;
 krita)
-    exec "/opt/appimage/krita-5.2.0-x86_64.appimage" "${@}"
+    exec "/opt/appimage/krita-5.2.1-x86_64.appimage" "${@}"
     ;;
 inkscape)
     exec "/opt/appimage/Inkscape-091e20e-x86_64.AppImage" "${@}"
     ;;
 bruno)
-    exec "/opt/appimage/bruno_1.2.0_x86_64_linux.AppImage" "${@}"
+    exec "/opt/appimage/bruno_1.3.0_x86_64_linux.AppImage" "${@}"
     ;;
 "$(_script_fn)")
     _echo "Symlink the script to the app, e.g. ln -s $(_script_fp) /usr/bin/onlyoffice-desktopeditors"
