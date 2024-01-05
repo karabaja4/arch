77b527c3253c967f1450f9c84b5b1c4e917f40f5
karabaja4 <karabaja4@gmail.com>
2024-01-05 21:07:43 +0100

    a

--- linode/rent.sh
+++ linode/rent.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-set -u
+set -eu
+
+_echo() {
+    printf '%s %s\n' "[$(date -Is)]" "${1}"
+}
 
 _auth="$(jq -crM '.authorizationKey' "/var/www/discordapi/src/config.json")"
 if [ -n "${_auth}" ]
 then
+    _echo "Sending alert to Discord"
     curl -s -X POST 'https://avacyn.radiance.hr/discord/alerts' \
         --header "Authorization: ${_auth}" \
         --header 'Content-Type: application/json' \
-        --data-raw "{\"text\": \"Uplati si stanarinu sa firme!\"}"
+        --data-raw "{\"text\": \"Uplati si stanarinu sa firme za $(date +%-m). mjesec!\"}"
+    printf '\n'
+    _echo "Exited with ${?}"
 fi
