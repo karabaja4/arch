271872137f257def563118db4f76caac5796ce06
karabaja4 <karabaja4@gmail.com>
2024-01-05 21:10:50 +0100

    a

--- linode/mount.sh
+++ linode/mount.sh
@@ -9,5 +9,8 @@ _mount() {
     mount -t cifs -o credentials=/etc/smbcredentials/linode.cred,uid="${_uid}",gid="${_gid}",dir_mode=0755,file_mode=0644,serverino,nosharesock,actimeo=30 "${@}"
 }
 
-_mount "//linode.file.core.windows.net/public1" "/var/www/_public"
-_mount "//linode.file.core.windows.net/private1" "/home/${_user}/_private"
+# public mounts
+_mount "//linode.file.core.windows.net/hacker" "/var/www/public/hacker"
+
+# private mounts
+_mount "//linode.file.core.windows.net/azure" "/var/samba/private/azure"
