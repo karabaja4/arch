83d290fdd1f0384ec3ac18189a78ad016173ba9c
karabaja4 <karabaja4@gmail.com>
2024-01-13 17:17:34 +0100

    a

--- linode/certbot/alert.sh
+++ linode/certbot/alert.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -u
 
-_auth="$(jq -crM '.authorizationKey' "/var/www/discordapi/src/config.json")"
+_auth="$(jq -crM '.endpoints[] | select(.route=="/alerts") | .authorization' "/var/www/discordapi/src/config.json")"
 if [ -n "${_auth}" ]
 then
-    curl -s -X POST 'https://avacyn.radiance.hr/discord/alerts' \
+    curl -s -X POST 'https://api.radiance.hr/alerts' \
         --header "Authorization: ${_auth}" \
         --header 'Content-Type: application/json' \
-        --data-raw "{\"text\": \"Certbot reneweal complete!\nDirectory: ${RENEWED_LINEAGE}\nDomains: ${RENEWED_DOMAINS}\"}"
+        --data-raw "{\"text\": \"Certbot reneweal complete!\nDirectory: ${RENEWED_LINEAGE:-"NOTSET"}\nDomains: ${RENEWED_DOMAINS:-"NOTSET"}\"}"
 fi
