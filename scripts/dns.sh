a25606ee668529413b9c8d79a5e8e6924eff5127
karabaja4 <karabaja4@gmail.com>
2024-05-13 01:33:04 +0200

    a

--- scripts/dns.sh
+++ scripts/dns.sh
@@ -5,34 +5,21 @@ _fn="$(basename "${0}")"
 _fd="$(dirname "$(readlink -f "${0}")")"
 
 _logfile="${_fd}/dns.log"
+_secret="/etc/secret/secret.json"
+
+_record="$(jq -r ".dns .record" "${_secret}")"
+_token="$(jq -r ".dns .token" "${_secret}")"
+_ctype="Content-Type: application/json"
+_auth="Authorization: Bearer ${_token}"
 
 _echo() {
     printf '[\033[35m%s\033[0m] %s\n' "${_fn}" "${1}"
     printf '[%s][%s] %s\n' "${_fn}" "$(date -Is)" "${1}" >> "${_logfile}"
 }
 
-# clean up after file gets over 50MB, keep last 6000 lines
-_logsize="$(stat -c %s "${_logfile}" 2>/dev/null || printf '%s\n' 0)"
-if [ "${_logsize}" -gt 52428800 ]
-then
-    tail -n 6000 "${_logfile}" > "${_logfile}.temp"
-    mv "${_logfile}.temp" "${_logfile}"
-    _echo "Cleaned up log file."
-fi
-
-_secret="/etc/secret/secret.json"
-_record=$(jq -r ".dns .record" "${_secret}")
-_token=$(jq -r ".dns .token" "${_secret}")
-
-_echo "Secret: ${_secret}"
-_echo "Record: ${_record}"
-
 _get_current_ip() (
     curl -s -f "https://avacyn.radiance.hr/ip" || exit 1
 )
-
-_ctype="Content-Type: application/json"
-_auth="Authorization: Bearer ${_token}"
 
 _get_do_ip() (
     _result="$(curl -s -f -X GET -H "${_ctype}" -H "${_auth}" "${_record}")" || exit 2
@@ -49,7 +36,21 @@ _exit() {
     exit "${1}"
 }
 
+# clean up after file gets over 50MB, keep last 6000 lines
+_cleanup() {
+    _logsize="$(stat -c %s "${_logfile}" 2>/dev/null || printf '%s\n' 0)"
+    if [ "${_logsize}" -gt 52428800 ]
+    then
+        tail -n 6000 "${_logfile}" > "${_logfile}.temp"
+        mv "${_logfile}.temp" "${_logfile}"
+        _echo "Cleaned up log file."
+    fi
+}
+
 _run() {
+    _echo "Secret: ${_secret}"
+    _echo "Record: ${_record}"
+
     _current_ip="$(_get_current_ip)" || _exit "${?}"
     _do_ip="$(_get_do_ip)" || _exit "${?}"
 
@@ -66,4 +67,5 @@ _run() {
     _exit 0
 }
 
+_cleanup
 _run
