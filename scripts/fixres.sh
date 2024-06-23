115e1cc2c65a316813a8bba197854bd38431f6cf
Igor Saric <karabaja4@gmail.com>
2024-06-23 23:59:50 +0200

    a

--- scripts/fixres.sh
+++ scripts/fixres.sh
@@ -8,12 +8,49 @@ _res_middle="1920x1200"
 _res_right="1920x1200"
 _res_left="2560x1600"
 
+_exists() {
+    _screen="${1}"
+    _test="$(xrandr --output "${_screen}" 2>&1)"
+    if [ -z "${_test}" ]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+_log() {
+    printf 'Setting %s to %s\n' "${1}" "${2}"
+}
+
 # middle
-xrandr --output "${_screen_middle}" --mode "${_res_middle}" --primary
+if _exists "${_screen_middle}"
+then
+    _log "${_screen_middle}" "${_res_middle}"
+    xrandr --output "${_screen_middle}" --mode "${_res_middle}" --primary
+fi
 
 # right
-xrandr --addmode "${_screen_right}" "${_res_right}"
-xrandr --output "${_screen_right}" --mode "${_res_right}" --right-of "${_screen_middle}"
+if _exists "${_screen_right}"
+then
+    _log "${_screen_right}" "${_res_right}"
+    #xrandr --addmode "${_screen_right}" "${_res_right}"
+    if _exists "${_screen_middle}"
+    then
+        xrandr --output "${_screen_right}" --mode "${_res_right}" --right-of "${_screen_middle}"
+    else
+        xrandr --output "${_screen_right}" --mode "${_res_right}"
+    fi
+fi
 
 # left
-xrandr --output "${_screen_left}" --mode "${_res_left}" --left-of "${_screen_middle}"
+if _exists "${_screen_left}"
+then
+    _log "${_screen_left}" "${_res_left}"
+    if _exists "${_screen_middle}"
+    then
+        xrandr --output "${_screen_left}" --mode "${_res_left}" --left-of "${_screen_middle}"
+    else
+        xrandr --output "${_screen_left}" --mode "${_res_left}"
+    fi
+fi
