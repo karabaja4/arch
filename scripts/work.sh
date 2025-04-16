00acf0280d762271874ebdbcd0ca9f691b0ff469
Igor Saric <karabaja4@gmail.com>
2025-04-17 01:23:15 +0200

    a

--- scripts/work.sh
+++ scripts/work.sh
@@ -11,7 +11,7 @@ _user="$(jq -crM '.work.user' "${_secret}")"
 _password="$(jq -crM '.work.password' "${_secret}")"
 
 _color_echo 31 "Connecting to: ${_user} @ ${_domain}/${_host}"
-exec xfreerdp /monitors:0 /cert-ignore /bpp:32 /network:lan /audio-mode:2 /scale:140 /multimon /floatbar:sticky:off /gfx:RFX -themes -wallpaper \
+exec xfreerdp /monitors:0 /cert-ignore /bpp:32 /network:lan /audio-mode:2 /scale:100 /multimon /floatbar:sticky:off /gfx:RFX -themes -wallpaper \
 /d:"${_domain}" \
 /v:"${_host}" \
 /u:"${_user}" \
