87b42b2f7b0ef3329930a2efd93882249c94b0da
Igor Saric <karabaja4@gmail.com>
2025-06-14 21:58:57 +0200

    a

--- scripts/dpms.sh
+++ scripts/dpms.sh
@@ -6,32 +6,34 @@ export DISPLAY=":1"
 
 xset dpms 0 0 0
 
-# Initialize flags
-_audio_running=0
-_vbox_running=0
-
-# Check if audio is running
-if grep -q "RUNNING" /proc/asound/card*/pcm*/sub*/status 2>/dev/null
-then
-    _audio_running=1
-fi
-
-# Check if VirtualBox is running
-if wmctrl -l 2>/dev/null | grep -q "\[Running\] - Oracle VirtualBox"
-then
-    _vbox_running=1
-fi
-
 # Get idle time in milliseconds
 _idle_time="$(xprintidle)"
 
 # Turn off screen based on conditions
-if [ "${_audio_running}" -eq 1 ] && [ "${_vbox_running}" -eq 1 ]
+
+if [ "${_idle_time}" -gt 300000 ]
 then
-    xset dpms force off
-elif [ "${_audio_running}" -eq 0 ] && [ "${_idle_time}" -gt 300000 ]
-then
-    xset dpms force off
-else
-    printf 'Audio: %s, Virtualbox: %s, Idle: %s\n' "${_audio_running}" "${_vbox_running}" "${_idle_time}"
+    # Initialize flags
+    _audio_running=0
+    _vbox_running=0
+
+    # Check if audio is running
+    if grep -q "RUNNING" /proc/asound/card*/pcm*/sub*/status 2>/dev/null
+    then
+        _audio_running=1
+    fi
+
+    # Check if VirtualBox is running
+    if wmctrl -l 2>/dev/null | grep -q "\[Running\] - Oracle VirtualBox"
+    then
+        _vbox_running=1
+    fi
+
+    if [ "${_audio_running}" -eq 1 ] && [ "${_vbox_running}" -eq 1 ]
+    then
+        xset dpms force off
+    elif [ "${_audio_running}" -eq 0 ]
+    then
+        xset dpms force off
+    fi
 fi
