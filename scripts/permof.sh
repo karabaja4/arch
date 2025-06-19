ee8057337b9ad60bff6b44bf6209cedd128d0ece
Igor Saric <karabaja4@gmail.com>
2025-06-19 18:27:38 +0200

    a

--- scripts/permof.sh
+++ scripts/permof.sh
@@ -3,41 +3,64 @@ set -eu
 IFS='
 '
 
-_echo() {
-    printf '%s\n' "${@}"
+_is_permission_value() {
+    case "${1}" in
+        [0-7][0-7][0-7][0-7])
+        return 0
+        ;;
+    *)
+        return 1
+        ;;
+    esac
 }
-
-_usage() {
-    _echo \
-    "permof 1.0 - visualize permissions of a file or a directory." \
-    "usage: $(basename "${0}") [FILE]..."
-    exit 1
-}
-
-[ "${#}" -lt 1 ] && _usage
-
 
 _byte_to_bin() {
-    _echo "${1}" | cut -c "${2}" | _echo "obase=2;$(cat -)" | bc | xargs printf '%03d\n'
+    printf '%s\n' "${1}" | cut -c "${2}" | printf '%s\n' "obase=2;$(cat -)" | bc | xargs printf '%03d\n'
 }
 
 _get_bit() {
-    _bit="$(_echo "${1}" | cut -c "${2}")"
+    _bit="$(printf '%s' "${1}" | cut -c "${2}")"
     if [ "${_bit}" = "1" ]
     then
-        _echo "X"
+        printf 'X'
     else
-        _echo " "
+        printf ' '
     fi
 }
 
 _output() {
-printf "
-  \033[37m#\033[0m Filename: %s
-  \033[37m#\033[0m Filetype: %s
-  \033[37m#\033[0m Permissions: \033[32m%s\033[0m
-  \033[37m#\033[0m Owner: \033[36m%s\033[0m
-
+    
+    printf '\n'
+    
+    # $1 - filename
+    if [ -n "${1}" ]
+    then
+        printf "  \033[37m#\033[0m Filename: %s\n" "${1}"
+    fi
+    
+    # $2 - filetype
+    if [ -n "${2}" ]
+    then
+        printf "  \033[37m#\033[0m Filetype: %s\n" "${2}"
+    fi
+    
+    # $3 - permissions
+    if [ -n "${3}" ]
+    then
+        printf "  \033[37m#\033[0m Permissions: \033[32m%s\033[0m\n" "${3}"
+    fi
+    
+    # $4 - owner
+    if [ -n "${4}" ]
+    then
+        printf "  \033[37m#\033[0m Owner: \033[36m%s\033[0m\n" "${4}"
+    fi
+    
+    # table
+    if [ -n "${5}" ] && [ -n "${6}" ] && [ -n "${7}" ] && [ -n "${8}" ] && [ -n "${9}" ]  && [ -n "${10}" ] && \
+       [ -n "${11}" ] && [ -n "${12}" ] && [ -n "${13}" ] && [ -n "${14}" ] && [ -n "${15}" ] && [ -n "${16}" ]
+    then
+    printf "
           Read  Write Execute    Setuid Setgid Sticky
          ┌─────┬─────┬─────┐     ┌─────┬─────┬─────┐
   Owner  │  \033[32m%s\033[0m  │  \033[32m%s\033[0m  │  \033[32m%s\033[0m  │     │  \033[32m%s\033[0m  │  \033[32m%s\033[0m  │  \033[32m%s\033[0m  │
@@ -47,24 +70,15 @@ printf "
   Public │  \033[32m%s\033[0m  │  \033[32m%s\033[0m  │  \033[32m%s\033[0m  │
          └─────┴─────┴─────┘
 
-" \
-"${1}" "${2}" "${3}" "${4}" \
-"${5}" "${6}" "${7}" "${8}" "${9}" "${10}" \
-"${11}" "${12}" "${13}" \
-"${14}" "${15}" "${16}"
+" "${5}" "${6}" "${7}" "${8}" "${9}" "${10}" "${11}" "${12}" "${13}" "${14}" "${15}" "${16}"
+    fi
 }
 
-for _item in "${@}"
-do
-    _filename="$(stat -c '%n' "${_item}")"
-    _filetype="$(stat -c '%F' "${_item}")"
-    _owner="$(stat -c '%U:%G' "${_item}")"
-    _permissions="$(stat -c '%04a' "${_item}")"
-    
-    _byte1="$(_byte_to_bin "${_permissions}" 1)"
-    _byte2="$(_byte_to_bin "${_permissions}" 2)"
-    _byte3="$(_byte_to_bin "${_permissions}" 3)"
-    _byte4="$(_byte_to_bin "${_permissions}" 4)"
+_print_permissions() {
+    _byte1="$(_byte_to_bin "${3}" 1)"
+    _byte2="$(_byte_to_bin "${3}" 2)"
+    _byte3="$(_byte_to_bin "${3}" 3)"
+    _byte4="$(_byte_to_bin "${3}" 4)"
 
     _setuid="$(_get_bit "${_byte1}" 1)"
     _setgid="$(_get_bit "${_byte1}" 2)"
@@ -82,8 +96,56 @@ do
     _public_write="$(_get_bit "${_byte4}" 2)"
     _public_exec="$(_get_bit "${_byte4}" 3)"
 
-    _output "${_filename}" "${_filetype}" "${_permissions}" "${_owner}" \
+    _output "${1}" "${2}" "${3}" "${4}" \
             "${_owner_read}" "${_owner_write}" "${_owner_exec}" "${_setuid}" "${_setgid}" "${_sticky}" \
             "${_group_read}" "${_group_write}" "${_group_exec}" \
             "${_public_read}" "${_public_write}" "${_public_exec}"
-done
+}
+
+_usage() {
+    printf "permof 1.1 - visualize permissions of a file or a directory.
+usage: %s [-h] [-q MODE] [FILE]...
+  --help, -h        Show this help list.
+  --query, -q MODE  Show visualization for the provided octal permission (e.g. 0755)
+" "$(basename "${0}")"
+    exit 1
+}
+
+_error() {
+    printf "\033[91mERROR: %s\033[0m\n" "${1}"
+}
+
+if [ "${#}" -eq 0 ]
+then
+    _usage
+elif [ "${#}" -eq 1 ] && { [ "${1}" = "-h" ] || [ "${1}" = "--help" ]; }
+then
+    _usage
+elif [ "${#}" -eq 2 ] && { [ "${1}" = "-q" ] || [ "${1}" = "--query" ]; }
+then
+    if _is_permission_value "${2}"
+    then
+        _print_permissions "" "" "${2}" ""
+    else
+        _error "${2} is not a valid permission value, must be an octal 4 digit value."
+        exit 1
+    fi
+else
+    for _item in "${@}"
+    do
+        if [ -f "${_item}" ] || [ -d "${_item}" ]
+        then
+            # filename
+            # filetype
+            # permissions
+            # owner
+            _print_permissions \
+                "$(stat -c '%n' "${_item}")" \
+                "$(stat -c '%F' "${_item}")" \
+                "$(stat -c '%04a' "${_item}")" \
+                "$(stat -c '%U:%G' "${_item}")"
+        else
+            _error "${_item} is not a file or directory"
+        fi
+    done
+fi
