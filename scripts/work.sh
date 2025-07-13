981a8eead34c455e79431a1d650cc38ff9a5835f
Igor Saric <karabaja4@gmail.com>
2025-07-13 03:15:25 +0200

    a

--- scripts/work.sh
+++ scripts/work.sh
@@ -19,8 +19,12 @@ export LD_LIBRARY_PATH="/usr/lib/apulse${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 # /gfx:AVC444
 # /gfx:RFX
 
+# The client supports version 0xA0701 of the RDP graphics protocol, client mode: 2, AVC available: 1, Initial profile: 2. Server: XXX // AVC444
+# The client supports version 0xA0701 of the RDP graphics protocol, client mode: 2, AVC available: 0, Initial profile: 2. Server: XXX // RFX
+# The client supports RDP 7.1 or lower protocol. Server: XXX
+
 _color_echo 94 "Connecting to: ${_user} @ ${_domain}/${_host}"
-exec xfreerdp3 /cert:ignore /bpp:32 /network:lan /audio-mode:0 /sound:sys:pulse /scale:100 /monitors:1 /floatbar:sticky:off /gfx:RFX /log-level:FATAL +f -themes -wallpaper -grab-keyboard \
+exec xfreerdp3 /cert:ignore /bpp:32 /network:lan /audio-mode:0 /sound:sys:pulse /scale:100 /monitors:1 /floatbar:sticky:off /log-level:FATAL +f -themes -wallpaper -grab-keyboard \
 /d:"${_domain}" \
 /v:"${_host}" \
 /u:"${_user}" \
