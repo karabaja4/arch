47702d57190730334bb91650f1df0b5241efefda
Igor Saric <karabaja4@gmail.com>
2025-07-14 02:31:25 +0200

    a

--- scripts/work.sh
+++ scripts/work.sh
@@ -21,8 +21,11 @@ export LD_LIBRARY_PATH="/usr/lib/apulse${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
 # The client supports version 0xA0701 of the RDP graphics protocol, client mode: 2, AVC available: 1, Initial profile: 2. Server: XXX // AVC444
 # The client supports version 0xA0701 of the RDP graphics protocol, client mode: 2, AVC available: 0, Initial profile: 2. Server: XXX // RFX
-# The client supports RDP 7.1 or lower protocol. Server: XXX
-# The server security layer detected an error (0x800903304) in the protocol stream and the client (Client IP: 192.168.100.XX) has been disconnected.
+# The client supports RDP 7.1 or lower protocol. Server: XXX // no param
+# The client supports version 0x80105 of the RDP graphics protocol, client mode: 0, AVC available: 0, Initial profile: 2. Server: XXX // Win 8.1
+
+# WinXP
+# The server security layer detected an error (0x80090304) in the protocol stream and the client (Client IP: 192.168.100.XX) has been disconnected.
 # RDP_SEC: An error was encountered when transitioning from FStateActivated in response to FEventHandshakeFailed (error code 0x8007052E)
 
 _color_echo 94 "Connecting to: ${_user} @ ${_domain}/${_host}"
