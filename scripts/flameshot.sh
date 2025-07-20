e498b6657315747607cd6c54855746e55286a9da
Igor Saric <karabaja4@gmail.com>
2025-07-20 15:45:59 +0200

    a

--- scripts/flameshot.sh
+++ scripts/flameshot.sh
@@ -7,5 +7,16 @@ flameshot gui -r > "${_path}"
 
 if [ -s "${_path}" ]
 then
-    xclip -in -selection clipboard -t 'image/png' "${_path}"
+    _wmout="$(wmctrl -l 2>/dev/null)"
+    _type='image/png'
+
+    # virtualbox only supports bmp
+    case "${_wmout}" in
+    *'FreeRDP:'* | *'[Running] - Oracle VirtualBox'* )
+        magick "${_path}" "bmp:${_path}"
+        _type='image/bmp'
+        ;;
+    esac
+
+    xclip -in -selection clipboard -t "${_type}" "${_path}"
 fi
