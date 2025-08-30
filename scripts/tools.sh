85fceca36e92a78b50a88689b5249e23fcfc9c3f
Igor Saric <karabaja4@gmail.com>
2025-08-30 04:42:03 +0200

    a

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -3,17 +3,17 @@ set -u
 
 _uid="1000"
 _gid="1000"
-_secret="/home/igor/secret.txt"
+_secret="/root/secret.txt"
 _username="$(cat "${_secret}" | sed -n '1p')"
 _password="$(cat "${_secret}" | sed -n '2p')"
-_public="/home/igor/public"
-_private="/home/igor/private"
+_public="/root/public"
+_private="/root/private"
 
 _mount_remote() {
     mount -t cifs -o username="${_username}",password="${_password}",uid="${_uid}",gid="${_gid}",dir_mode=0755,file_mode=0644,port=44555 "${@}"
 }
 
-mountremote() {
+mountall() {
     mkdir -p "${_public}"
     mkdir -p "${_private}"
     _mount_remote "//radiance.hr/public" "${_public}"
@@ -32,7 +32,7 @@ zero() {
 backup() {
     if [ -z "${1-}" ] || [ -z "${2-}" ] || [ ! -b "${1}" ]
     then
-        printf '%s\n' "Usage example: backup /dev/nvme0n1 /home/igor/private/backups/win" >&2
+        printf '%s\n' "Usage example: backup /dev/nvme0n1 /root/private/backups/win" >&2
         return 1
     fi
     dd if="${1}" conv=sync,noerror bs=64K | gzip -c > "${2}.img.gz"
@@ -41,7 +41,7 @@ backup() {
 restore() {
     if [ -z "${1-}" ] || [ -z "${2-}" ] || [ ! -b "${2}" ]
     then
-        printf '%s\n' "Usage example: restore /home/igor/private/backups/win.img.gz /dev/nvme0n1" >&2
+        printf '%s\n' "Usage example: restore /root/private/backups/win.img.gz /dev/nvme0n1" >&2
         return 1
     fi
     gunzip -c "${1}" | dd of="${2}"
