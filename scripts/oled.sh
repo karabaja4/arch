6863c5ecdc337fa666263d35fdb359483c78349b
Igor Saric <karabaja4@gmail.com>
2025-09-03 19:48:11 +0200

    a

--- scripts/oled.sh
+++ scripts/oled.sh
@@ -5,9 +5,9 @@ _py="oled.py"
 _root="$(dirname "$(readlink -f "$0")")"
 _pids="$(pgrep -fx "python3 ${_root}/${_py} -m")"
 
-if [ -n "${_pids}" ]
+if [ -z "${_pids}" ]
 then
-    printf '%s\n' "${_pids}" | xargs -n1 kill
-else
     ( python3 "${_root}/${_py}" -m & ) > /dev/null 2>&1
+else
+    printf '%s\n' "${_pids}" | xargs -n1 kill
 fi
