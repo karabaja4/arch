3c3b0c4e57e4326889dfd10ab989553758a878e4
Igor Saric <karabaja4@gmail.com>
2025-09-07 01:36:42 +0200

    a

--- scripts/torrent.sh
+++ scripts/torrent.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 . "/home/igor/arch/scripts/_lib.sh"
+set -e
+
+case "${_arg1}" in
+    *.torrent) 
+        ;;
+    *)
+        _fatal "${_arg1} is not a .torrent file."
+        ;;
+esac
 
 if [ -f "${_arg1}" ]
 then
@@ -9,20 +18,24 @@ then
         _drop="${_disk}/drop"
         if [ -d "${_drop}" ]
         then
-            # this will append .moving suffix to a file
-            # so multiple instances of the script see only one file
+            # temp file
             _moving_file="${_arg1}.moving"
             if [ -e "${_moving_file}" ]
             then
                 _fatal "${_moving_file} exists."
             fi
-            if mv "${_arg1}" "${_moving_file}"
+            mv "${_arg1}" "${_moving_file}"
+
+            # temp file to drop file
+            _fn="$(basename "${_arg1}")"
+            if [ -e "${_drop}/${_fn}" ]
             then
-                if mv "${_moving_file}" "${_drop}/"
-                then
-                    _herbe "Moved \"$(basename "${_arg1}")\" to \"${_drop}\""
-                fi
+                _fatal "${_drop}/${_fn} already exists."
             fi
+            mv "${_moving_file}" "${_drop}/${_fn}"
+
+            # notification
+            _herbe "Moved \"${_fn}\" to \"${_drop}\""
         fi
     fi
 fi
