a241a7a42269a6e7213b640c98a0fd07a1e5a3d2
Igor Saric <karabaja4@gmail.com>
2025-09-07 00:21:47 +0200

    a

--- scripts/vmware.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-modprobe -a vmw_vmci vmmon vmnet
-
-#/usr/bin/vmware-networks --postinstall vmware-player,0,1
-/usr/bin/vmware-networks --start
-/usr/lib/vmware/bin/vmware-usbarbitrator -f
