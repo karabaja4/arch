1169ce258aa427947dffc04249131437e79f0d26
Igor Saric <karabaja4@gmail.com>
2025-09-07 21:32:49 +0200

    a

--- scripts/fixres.sh
+++ scripts/fixres.sh
@@ -1,23 +1,67 @@
 #!/bin/sh
-set -u
+set -eu
 
-pkill -f conkyrc-kernel
+### configuration ###
 
-_external_resolution='3840x2160'
-_laptop_resolution='2560x1600'
+_rtl_wallpapers="
+${HOME}/arch/wall/exodus_v01_5120x2880.png
+${HOME}/arch/wall/exodus_v03_5120x2880.png
+${HOME}/arch/wall/exodus_v02_5120x2880.png
+${HOME}/arch/wall/exodus_v04_5120x2880.png
+${HOME}/arch/wall/exodus_v05_5120x2880.png
+${HOME}/arch/wall/exodus_v06_5120x2880.png
+"
 
-_connected="$(xrandr | awk '/ connected / { print; getline; print }')"
-_xrandr_count=0
+_rtl_monitors="
+ATNA60HU01-0
+PG32UCDM
+"
 
-_get_screen_info_by_resolution() {
-    printf '%s' "${_connected}" | grep -B1 "^   ${1}"
+_primary_monitor="PG32UCDM"
+
+#####################
+
+# remove empty lines
+_rtl_wallpapers="$(printf '%s\n' "${_rtl_wallpapers}" | grep '.')"
+_rtl_monitors="$(printf '%s\n' "${_rtl_monitors}" | grep '.')"
+
+# kill all conky instances
+pkill -f conkyrc-kernel || true
+
+_get_wallpaper_for_line() {
+    printf '%s\n' "${_rtl_wallpapers}" | sed -n "${1}p"
 }
 
-_get_screen_name() {
-    printf '%s' "${1}" | awk 'NR==1 { print $1 }'
+# use edid data to read monitor model
+_get_monitor_to_port_map() {
+    xrandr --props | awk '/ connected / {d=$1} /EDID:/ {h=""; while (getline) {if ($0 !~ /^[ \t]*[0-9a-f]+$/) break; gsub(/[ \t]/,""); h=h $0} if(h!="") print d, h}' | \
+    while read -r _line
+    do
+        _port="$(printf "%s" "${_line}" | cut -d' ' -f1)"
+        _edid_data="$(printf "%s" "${_line}" | cut -d' ' -f2-)"
+        _product_name="$(printf '%s\n' "${_edid_data}" | edid-decode | grep "Display Product Name:" | sed "s/.*'\(.*\)'.*/\1/" | awk '{$1=$1; print}')"
+        printf "%s %s\n" "${_product_name}" "${_port}" 
+    done
 }
 
-_get_max_refresh_rate() {
+# map "<monitor name>" "<port name>"
+_monitor_to_port_map="$(_get_monitor_to_port_map)"
+printf '%s\n' "Monitor to Port map:"
+printf '%s\n' "${_monitor_to_port_map}" | sed 's/^/-> /'
+_get_port_for_monitor() {
+    printf '%s\n' "${_monitor_to_port_map}" | grep "^${1} " | cut -d' ' -f2
+}
+
+_all_screen_infos="$(xrandr | awk '/ connected / { print; getline; print }')"
+_get_screen_info_for_port() {
+    printf '%s' "${_all_screen_infos}" | grep -A1 "^${1} connected"
+}
+
+_get_max_resolution_for_screen_info() {
+    printf '%s' "${1}" | awk 'NR==2 {print $1}'
+}
+
+_get_max_refresh_rate_for_screen_info() {
     printf '%s' "${1}" | awk 'NR==2' | tr -d '+*' | awk '{$1=""; sub(/^ /, ""); print}' | tr ' ' '\n' | sort -nr | head -n1
 }
 
@@ -26,51 +70,57 @@ _set_wallpaper() {
     xwallpaper --output "${1}" --stretch "${2}"
 }
 
-_set_xrandr() {
-    _set_xrandr_output="${1}"
-    _set_xrandr_mode="${2}"
-    _set_xrandr_rate="${3}"
-    shift 3
-    printf 'Setting %s to %s@%s\n' "${_set_xrandr_output}" "${_set_xrandr_mode}" "${_set_xrandr_rate}"
-    xrandr --output "${_set_xrandr_output}" --mode "${_set_xrandr_mode}" --rate "${_set_xrandr_rate}" "${@}"
-    _xrandr_count=$((_xrandr_count + 1))
+_start_conky_on_index() {
+    printf 'Starting conky on screen %s\n' "${1}"
+    conky -q -d -c "${HOME}/arch/conky/conkyrc-kernel" --xinerama-head "${1}"
 }
 
-_laptop_info="$(_get_screen_info_by_resolution "${_laptop_resolution}")"
+# iterate monitors right to left
+_configure_screens()
+{
+    _i=0
+    _previous_port=""
+    for _monitor in ${_rtl_monitors}
+    do
+        printf 'Processing Monitor: %s\n' "${_monitor}"
+        _port="$(_get_port_for_monitor "${_monitor}")"
+        if [ -z "${_port}" ]
+        then
+            printf 'Cannot find port for monitor: %s\n' "${_monitor}"
+            exit 1
+        fi
+        _screen_info="$(_get_screen_info_for_port "${_port}")"
+        _max_resolution="$(_get_max_resolution_for_screen_info "${_screen_info}")"
+        _max_refresh_rate="$(_get_max_refresh_rate_for_screen_info "${_screen_info}")"
+        
+        set -- --output "${_port}" --mode "${_max_resolution}" --rate "${_max_refresh_rate}"
 
-# laptop display is mandatory
-if [ -z "${_laptop_info}" ]
-then
-    printf '%s\n' "Unable to detect internal laptop display."
-    exit 1
-fi
+        if [ "${_monitor}" = "${_primary_monitor}" ]
+        then
+            set -- "${@}" --primary
+        fi
+        
+        if [ "${_i}" -gt 0 ]
+        then
+            set -- "${@}" --right-of "${_previous_port}"
+        fi
+        
+        printf "xrandr %s\n" "${*}"
+        
+        xrandr "${@}"
+        _start_conky_on_index "${_i}"
+        
+        _wallpaper_path="$(_get_wallpaper_for_line "$((_i + 1))")"
+        if [ -n "${_wallpaper_path}" ]
+        then
+            _set_wallpaper "${_port}" "${_wallpaper_path}"
+        else
+            printf 'There are not enough wallpapers for %s\n' "${_port}"
+        fi
 
-_laptop_screen_name="$(_get_screen_name "${_laptop_info}")"
-_laptop_max_refresh_rate="$(_get_max_refresh_rate "${_laptop_info}")"
+        _previous_port="${_port}"
+        _i=$((_i + 1))
+    done
+}
 
-_external_info="$(_get_screen_info_by_resolution "${_external_resolution}")"
-if [ -n "${_external_info}" ]
-then
-    # external display connected, it's primary, laptop is left of external display
-    _external_screen_name="$(_get_screen_name "${_external_info}")"
-    _external_max_refresh_rate="$(_get_max_refresh_rate "${_external_info}")"
-    
-    _set_xrandr "${_external_screen_name}" "${_external_resolution}" "${_external_max_refresh_rate}" --primary
-    _set_xrandr "${_laptop_screen_name}" "${_laptop_resolution}" "${_laptop_max_refresh_rate}" --left-of "${_external_screen_name}"
-    
-    _set_wallpaper "${_external_screen_name}" "${HOME}/arch/wall/exodus_v03_5120x2880.png"
-    _set_wallpaper "${_laptop_screen_name}" "${HOME}/arch/wall/exodus_v01_5120x2880.png"
-else
-    # only laptop display, it's primary
-    _set_xrandr "${_laptop_screen_name}" "${_laptop_resolution}" "${_laptop_max_refresh_rate}" --primary
-    _set_wallpaper "${_laptop_screen_name}" "${HOME}/arch/wall/exodus_v03_5120x2880.png"
-fi
-
-# conky
-_i=0
-while [ "${_i}" -lt "${_xrandr_count}" ]
-do
-    printf '%s\n' "Starting conkyrc-kernel for monitor ${_i}"
-    conky -q -d -c "${HOME}/arch/conky/conkyrc-kernel" --xinerama-head "${_i}"
-    _i=$((_i + 1))
-done
+_configure_screens
