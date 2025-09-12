61f345e9bddd757025ad1b1bf092206490b4824a
Igor Saric <karabaja4@gmail.com>
2025-09-12 20:35:07 +0200

    a

--- scripts/cifswatch.sh
+++ scripts/cifswatch.sh
@@ -3,10 +3,9 @@
 # CIFS is unable to re-establish connection on SMB server restart
 # DebugData shows DISCONNECTED:
 
-# 1) \\radiance.hr\public Mounts: 1 DevInfo: 0x20 Attributes: 0x1006f
-# PathComponentMax: 255 Status: 3 type: DISK Serial Number: 0x1ef82e91
-# Share Capabilities: None Aligned, Partition Aligned,	Share Flags: 0x0
-# tid: 0xe63316b7	Optimal sector size: 0x200	Maximal Access: 0x1f01ff	DISCONNECTED
+# 1) \\radiance.hr\public
+# ...
+# ...	DISCONNECTED
 
 _root="$(dirname "$(readlink -f "${0}")")"
 _debug_data="$(cat /proc/fs/cifs/DebugData)"
