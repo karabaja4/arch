a3f87573cd2cc6b5668a79e1046cd371382be5ac
Igor Saric <karabaja4@gmail.com>
2025-09-17 02:36:23 +0200

    a

--- scripts/info.sh
+++ scripts/info.sh
@@ -13,7 +13,7 @@ printf '\n'
 printf '  %s @ %s\n\n' "$(cut -d '"' -f2 /etc/os-release | head -n1)" "$(cut -d' ' -f3 /proc/version)"
 printf '  * CPU:      %s\n' "$(grep 'model name' /proc/cpuinfo | head -n1 | sed 's/model name\t: //')"
 printf '  * Memory:   %.2f GB / %.2f GB\n' "${_mem_used_gb}" "${_mem_total_gb}"
-printf '  * Shell:    %s\n' "$(readlink /proc/$PPID/exe | sed 's/\/usr\/bin\///')"
+printf '  * Shell:    %s\n' "$(readlink /proc/${PPID}/exe | sed 's/\/usr\/bin\///')"
 printf '  * Uptime:   %s\n' "$(uptime -p | cut -c 4-)"
 printf '  * Packages: %s (%s AUR)\n' "$(pacman -Qq | wc -l)" "$(pacman -Qm | wc -l)"
 printf '\n'
