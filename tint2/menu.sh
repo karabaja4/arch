3bc99216998de9f972f6f09bb6ea22b94efedec5
Igor Saric <karabaja4@gmail.com>
2025-09-21 02:30:34 +0200

    a

--- tint2/menu.sh
+++ tint2/menu.sh
@@ -27,14 +27,14 @@ code)
     _run code
     ;;
 azuredatastudio)
-    _run azuredatastudio --disable-keytar --force-device-scale-factor=1.5
+    _run azuredatastudio --disable-keytar --force-device-scale-factor=1.75
     ;;
 virtualbox)
     _run VirtualBox
     ;;
 discord)
     _load_apulse
-    _run discord --disable-smooth-scrolling --force-device-scale-factor=1.5
+    _run discord --disable-smooth-scrolling --force-device-scale-factor=1.75
     ;;
 flameshot)
     _run /home/igor/arch/scripts/flameshot.sh
