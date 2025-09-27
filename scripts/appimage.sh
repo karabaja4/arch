4bd99ee97eb124f1d8af027bca76d64cd61ce73b
Igor Saric <karabaja4@gmail.com>
2025-09-28 00:54:06 +0200

    a

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -18,7 +18,7 @@ inkscape)
     exec "/opt/appimage/Inkscape-091e20e-x86_64.AppImage" "${@}"
     ;;
 bruno)
-    exec "/opt/appimage/bruno_2.10.0_x86_64_linux.AppImage" --force-device-scale-factor=1.5 "${@}"
+    exec "/opt/appimage/bruno_2.10.0_x86_64_linux.AppImage" --force-device-scale-factor=1.75 "${@}"
     ;;
 "${_script_filename}")
     _echo "Symlink the script to the app, e.g. ln -s ${_script_path} /usr/local/bin/krita"
