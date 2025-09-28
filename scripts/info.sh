a6cc3d59503c24dfad547a39e84438a0324ca51e
Igor Saric <karabaja4@gmail.com>
2025-09-28 03:48:47 +0200

    a

--- scripts/info.sh
+++ scripts/info.sh
@@ -3,14 +3,8 @@ set -eu
 
 clear
 
-_mem_total_kb="$(grep "^MemTotal:" /proc/meminfo | awk '{ print $2 }')"
-_mem_available_kb="$(grep "^MemAvailable:" /proc/meminfo | awk '{ print $2 }')"
-
-_mem_used_gb="$(printf '%s %s' "${_mem_total_kb}" "${_mem_available_kb}" | awk '{ print ($1 - $2) / 1048576 }')"
-_mem_total_gb="$(printf '%s' "${_mem_total_kb}" | awk '{ print $1 / 1048576 }')"
-
-_get_first_gpu() {
-    lspci | grep 'VGA' | head -n1 | awk -F'[][]' '{print $2}'
+_get_lspci_gpu() {
+    lspci | grep 'VGA' | head -n1 | awk -F'[][]' '{printf "\033[96m%s\033[0m\n", $2}'
 }
 
 _get_nvidia_gpu() {
@@ -18,7 +12,7 @@ _get_nvidia_gpu() {
     then
         if _nvidia_out="$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader 2>/dev/null)"
         then
-            printf '%s\n' "${_nvidia_out}"
+            printf '\033[91m%s\033[0m\n' "${_nvidia_out}"
         else
             return 1
         fi
@@ -27,17 +21,62 @@ _get_nvidia_gpu() {
     fi
 }
 
-_system_model="$(cat /sys/class/dmi/id/product_version)"
+_get_distro() {
+    cut -d '"' -f2 /etc/os-release | head -n1
+}
+
+_get_kernel() {
+    cut -d' ' -f3 /proc/version
+}
+
+_get_cpu() {
+    grep 'model name' /proc/cpuinfo | head -n1 | sed 's/model name\t: //'
+}
+
+_get_shell() {
+    readlink "/proc/${PPID}/exe" | sed 's/\/usr\/bin\///'
+}
+
+_get_package_count() {
+    pacman -Qq | wc -l
+}
+
+_get_aur_count() {
+    pacman -Qm | wc -l
+}
+
+_get_uptime() {
+    uptime -p | cut -c 4-
+}
+
+_mem_total_kb="$(grep "^MemTotal:" /proc/meminfo | awk '{ print $2 }')"
+_mem_available_kb="$(grep "^MemAvailable:" /proc/meminfo | awk '{ print $2 }')"
+
+_mem_used_gb="$(printf '%s %s' "${_mem_total_kb}" "${_mem_available_kb}" | awk '{ print ($1 - $2) / 1048576 }')"
+_mem_total_gb="$(printf '%s' "${_mem_total_kb}" | awk '{ print $1 / 1048576 }')"
+
+if [ -r '/sys/class/dmi/id/product_version' ] && grep -q ' ' '/sys/class/dmi/id/product_version' 2>/dev/null
+then
+    _system_model="$(cat '/sys/class/dmi/id/product_version')"
+elif [ -r '/sys/class/dmi/id/product_name' ]
+then
+    _system_model="$(cat '/sys/class/dmi/id/product_name')"
+else
+    _system_model=''
+fi
 
 printf '\n'
-printf '  %s\n' "${_system_model}"
-printf '  %s @ %s\n\n' "$(cut -d '"' -f2 /etc/os-release | head -n1)" "$(cut -d' ' -f3 /proc/version)"
-printf '  * CPU:      %s\n' "$(grep 'model name' /proc/cpuinfo | head -n1 | sed 's/model name\t: //')"
-printf '  * GPU:      %s\n' "$(_get_nvidia_gpu || _get_first_gpu)"
+if [ -n "${_system_model}" ]
+then
+    printf '  %s\n' "${_system_model}"
+fi
+printf '  %s @ %s\n\n' "$(_get_distro)" "$(_get_kernel)"
+printf '  * CPU:      %s\n' "$(_get_cpu)"
+printf '  * GPU:      %s\n' "$(_get_nvidia_gpu || _get_lspci_gpu)"
 printf '  * Memory:   %.2f GB / %.2f GB\n' "${_mem_used_gb}" "${_mem_total_gb}"
-printf '  * Shell:    %s\n' "$(readlink /proc/${PPID}/exe | sed 's/\/usr\/bin\///')"
-printf '  * Uptime:   %s\n' "$(uptime -p | cut -c 4-)"
-printf '  * Packages: %s (%s AUR)\n' "$(pacman -Qq | wc -l)" "$(pacman -Qm | wc -l)"
+printf '  * Shell:    %s\n' "$(_get_shell)"
+printf '  * Uptime:   %s\n' "$(_get_uptime)"
+printf '  * Packages: %s (%s AUR)\n' "$(_get_package_count)" "$(_get_aur_count)"
 printf '\n'
 
 printf '  '
