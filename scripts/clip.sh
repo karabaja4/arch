54d086724d88516b339d07c273b4ef677ab88e41
Igor Saric <karabaja4@gmail.com>
2025-10-11 23:05:26 +0200

    a

--- scripts/clip.sh
+++ scripts/clip.sh
@@ -33,7 +33,7 @@ _add_to_history() {
 }
 
 _random_digits() {
-    cat /dev/urandom | LC_ALL=C tr -dc '0-9' | fold -w "${1}" | head -n 1
+    LC_ALL=C tr -dc '0-9' < /dev/urandom | head -c "${1}"
 }
 
 _iteration() {
