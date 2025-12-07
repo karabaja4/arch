0561486188cbef4de985ab6200c490f8f8b87206
Igor Saric <karabaja4@gmail.com>
2025-12-07 21:05:44 +0100

    a

--- scripts/sloop.sh
+++ scripts/sloop.sh
@@ -45,7 +45,7 @@ else
 fi
 
 _color_echo 35 "Selecting: ${_to_select}"
-"${_root}"/sound.sh "${_to_select}" 85
+"${_root}"/sound.sh "${_to_select}" 80
 
 # send notification
 _herbe "Selected audio device: ${_to_select#*) }"
