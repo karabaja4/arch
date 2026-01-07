50f0043765cf5f3293ef29087c1b54e562d4e0c8
Igor Saric <karabaja4@gmail.com>
2026-01-08 00:27:01 +0100

    a

--- cron/log.js
+++ cron/log.js
@@ -1,7 +1,4 @@
 const fs = require('node:fs');
-const EventEmitter = require('node:events');
-const bus = new EventEmitter();
-
 const logpath = '/tmp/cron.log';
 
 const queue = [];
@@ -16,29 +13,31 @@ const unpack = (err) => {
   }
 };
 
-const process = async () => {
+const processQueue = async () => {
   if (processing) return;
   processing = true;
   try {
     while (queue.length > 0) {
       const item = queue.shift();
-      await fs.promises.appendFile(logpath, item);
-    };
-  } catch (err) {
-    console.log(`Error writing to log: ${unpack(err)}`);
+      try {
+        await fs.promises.appendFile(logpath, item);
+      } catch (err) {
+        console.log(`Error writing to log: ${unpack(err)}`);
+      }
+    }
   } finally {
     processing = false;
-    if (queue.length > 0) setImmediate(process);
+    // catch any logs added during finishing before processing was set to false
+    if (queue.length > 0) processQueue();
   }
 };
-bus.on('log-added', process);
 
 const push = (type, message) => {
   const utc = (new Date()).toISOString();
   const formatted = `[${utc}][${type}] ${unpack(message)}`;
   console.log(formatted);
   queue.push(`${formatted}\n`);
-  bus.emit('log-added');
+  processQueue();
 };
 
 module.exports = {
