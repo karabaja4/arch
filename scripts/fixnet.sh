4c28dc7b5f76b6eed365c41a9a4f4688e9b392bf
Igor Saric <karabaja4@gmail.com>
2026-02-24 00:38:12 +0100

    a

---
+++ scripts/fixnet.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -eu
+
+if [ "$(id -u)" -ne 0 ]
+then
+    printf "%s\n" "Must be root."
+    exit 1
+fi
+
+_device="8086:125c"
+_address="$(lspci -d "${_device}" | awk '{ print $1 }')"
+if [ -n "${_address}" ]
+then
+    _current="$(setpci -s "${_address}" B0.B)"
+    if [ "${_current}" = "42" ]
+    then
+        setpci -s "${_address}" b0.w=0040
+        printf '[%s] ASPM disabled.\n' "${_address}"
+    else
+        printf '[%s] Current value %s not valid, exiting.\n' "${_address}" "${_current}"
+    fi
+else
+    printf 'Device %s not found.\n' "${_device}"
+    exit 1
+fi
