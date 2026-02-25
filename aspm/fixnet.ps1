4e5ec91f8021de2cabc9af53be601f77bc84951f
Igor Saric <karabaja4@gmail.com>
2026-02-25 05:54:01 +0100

    a

--- aspm/fixnet.ps1
+++ aspm/fixnet.ps1
@@ -1,21 +1,41 @@
+$logFile = "C:\tools\fixnet.log"
+$rweExe = "C:\tools\rwe\Rw.exe"
+
+$deviceId = "0x125C8086"
+$registerOffset = "0xB0"
+$registerExpectedValue = "0x42"
+$registerWriteValue = "0x40"
+
 function Log ($message) {
     $timestamped = "[$(Get-Date -Format 'yyyy-MM-ddTHH:mm:ssK')] $message"
     Write-Host $timestamped
     $timestamped | Out-File -FilePath $logFile -Append
 }
 
-$logFile = "C:\tools\rwe\fixnet.log"
-$rwExe = "C:\tools\rwe\Rw.exe"
-$pciAddr = "0x81 0x00 0x00 0xB0"
-
-& $rwExe /Min /Nologo /Stdout /Command="RPCI $pciAddr" | Tee-Object -Variable tempOutput | Out-Null
-Log "Read output: $tempOutput"
-
-$value = ($tempOutput -split '=')[1].Trim()
-if ($value -eq '0x42') {
-    Log "Value is $value - writing..."
-    & $rwExe /Min /Nologo /Stdout /Command="WPCI $pciAddr 0x40" | Tee-Object -Variable tempOutput | Out-Null
-    Log "Write output: $tempOutput"
-} else {
-    Log "Value is $value - no action"
+function InvokeRwe ($command) {
+    Log "Command: $command"
+    $output = & $rweExe /Min /Nologo /Stdout /Command="$command" | Select-Object -First 1
+    $result = '0xFFFF'
+    if ($output -match ".+ = (0x[0-9A-Fa-f]+)") {
+        $result = $Matches[1]
+    }
+    Log "Result: $result"
+    return $result
 }
+
+Log "--- Start ---"
+
+$bdf = InvokeRwe "FPCI $deviceId 0"
+if ($bdf -ne '0xFFFF') {
+    $registerValue = InvokeRwe "eRPCI $bdf $registerOffset"
+    if ($registerValue -eq $registerExpectedValue) {
+        Log "Value $registerValue OK, writing $registerWriteValue"
+        $null = InvokeRwe "eWPCI $bdf $registerOffset $registerWriteValue"
+    } else {
+        Log "Value is $registerValue, expected $registerExpectedValue"
+    }
+} else {
+    Log "Device not found ($bdf)"
+}
+
+Log "--- End ---"
