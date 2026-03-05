d05eb17670f3582deb87c5fcbc7619805455ec54
Igor Saric <karabaja4@gmail.com>
2026-03-05 02:15:11 +0100

    a

---
+++ iptables/iptables.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Clear all existing rules in all chains
+iptables -F
+
+# Delete all user-defined chains
+iptables -X
+
+# Set default policies
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Allow established and related connections
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# Allow all traffic on the loopback interface
+iptables -A INPUT -i lo -j ACCEPT
+
