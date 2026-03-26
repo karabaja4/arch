6064c5889a917b98bcfd29a6638a0ce7d57c15b0
Igor Saric <karabaja4@gmail.com>
2026-03-26 18:47:58 +0100

    suspend crap

--- scripts/suspend.sh
+++ scripts/suspend.sh
@@ -17,11 +17,22 @@ _toggle_wakeup() {
     printf '%s\n' "${1}" > /proc/acpi/wakeup
 }
 
+_nvidia() {
+    if [ -x "/usr/bin/nvidia-sleep.sh" ]
+    then
+        /usr/bin/nvidia-sleep.sh "${1}"
+    fi
+}
+
 _toggle_wakeup "TXHC"
 _toggle_wakeup "XHCI"
 
+_nvidia "suspend"
+
 printf '%s\n' "deep" > /sys/power/mem_sleep
 printf '%s\n' "mem" > /sys/power/state
+
+_nvidia "resume"
 
 _toggle_wakeup "XHCI"
 _toggle_wakeup "TXHC"
