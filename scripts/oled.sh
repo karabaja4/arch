e26498c5a509d516ea061079281bd615bef41e8b
Igor Saric <karabaja4@gmail.com>
2026-06-06 04:32:10 +0200

    a

--- scripts/oled.sh
+++ scripts/oled.sh
@@ -10,17 +10,15 @@ _kill_on_hover() {
     # see if OLED saver is the first window under root, means we're hovering over the app
     if xwininfo -children -id "${_mouse_window_id}" | grep -q 'Window id: 0x.* "blackscreen"'
     then
-        _pid="$(cat /tmp/blackscreen.pid)"
-        [ -z "${_pid}" ] && return 1
-        kill "${_pid}"
+        xkill -id "${_mouse_window_id}"
         return 0
+    else
+        return 1
     fi
-    return 1
-    
 }
 
 if ! _kill_on_hover
 then
     # start oled saver detached under pid 1
-    ( /home/igor/arch/oled/blackscreen & printf '%s' "${!}" > /tmp/blackscreen.pid ) > /dev/null 2>&1
+    ( /home/igor/arch/oled/blackscreen & ) > /dev/null 2>&1
 fi
