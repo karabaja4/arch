cd7a99e25dc18342c09bb74a208fe890a5dd8b4d
Igor Saric <karabaja4@gmail.com>
2026-06-06 04:45:18 +0200

    a

--- scripts/oled.sh
+++ scripts/oled.sh
@@ -7,7 +7,7 @@ _kill_on_hover() {
     # get window id under mouse
     _mouse_window_id="$(xdotool getmouselocation --shell | awk -F= '/WINDOW/{print $2}')"
     
-    if xwininfo -children -id "${_mouse_window_id}" | grep -q 'Window id: 0x.* "blackscreen"'
+    if xwininfo -id "${_mouse_window_id}" | grep -q 'Window id: 0x.* "blackscreen"'
     then
         xkill -id "${_mouse_window_id}"
         return 0
