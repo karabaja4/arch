7a1c539799f59dceadde6f01823ff867b0b1ef96
Igor Saric <karabaja4@gmail.com>
2026-06-06 04:25:08 +0200

    a

--- scripts/oled.sh
+++ scripts/oled.sh
@@ -8,7 +8,7 @@ _kill_on_hover() {
     _mouse_window_id="$(xdotool getmouselocation --shell | awk -F= '/WINDOW/{print $2}')"
     
     # see if OLED saver is the first window under root, means we're hovering over the app
-    if xwininfo -children -id "${_mouse_window_id}" | grep -q 'Window id: 0x[0-9a-fA-F][0-9a-fA-F]* "blackscreen"'
+    if xwininfo -children -id "${_mouse_window_id}" | grep -q 'Window id: 0x.* "blackscreen"'
     then
         _pid="$(cat /tmp/blackscreen.pid)"
         [ -z "${_pid}" ] && return 1
