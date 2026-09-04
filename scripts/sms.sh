942b2ae7bb792868174d96521c9e0c370e2ca507
Igor Saric <karabaja4@gmail.com>
2026-09-05 00:43:39 +0200

    a

--- scripts/sms.sh
+++ scripts/sms.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-PORT=/dev/ttyUSB2
+_port='/dev/ttyUSB2'
 
-exec 3<"$PORT"
-exec 4>"$PORT"
+stty -F "${_port}" raw -echo
+
+exec 3<> "${_port}"
 
 _cmd()
 {
-    printf '%s\r' "$1" >&4
-    timeout 1 cat <&3
+    printf '%s\r' "${1}" >&3
+    timeout 0.2 cat <&3
 }
 
 _cmd 'AT'
 _cmd 'AT+CMGF=1'
 _cmd 'AT+CMGL="ALL"'
 _cmd 'AT+CMGD=0'
+
+# qmicli -d /dev/cdc-wdm0 --device-open-proxy --dms-swi-get-usb-composition
+# qmicli -d /dev/cdc-wdm0 --device-open-proxy --dms-swi-set-usb-composition=8
