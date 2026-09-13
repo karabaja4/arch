59918c97b89a67fe7349064d7fd69ec9c39c068c
Igor Saric <karabaja4@gmail.com>
2026-09-13 03:30:48 +0200

    a

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -140,4 +140,5 @@ fi
 
 wpa_supplicant -B -i "${_interface}" -c "${_config}"
 sleep 3
-udhcpc -nqfv -i "${_interface}"
+#udhcpc -nqfv -i "${_interface}"
+dhcpcd --oneshot --ipv4only "${_interface}"
