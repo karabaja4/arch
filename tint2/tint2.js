01fcd23025d37a0d57c0f30ddfc7042a83d50815
Igor Saric <karabaja4@gmail.com>
2026-09-16 18:40:56 +0200

    a

--- tint2/tint2.js
+++ tint2/tint2.js
@@ -285,7 +285,7 @@ const diskusage = async () => {
 };
 
 const weather = async () => {
-  const station = 'Zadar';
+  const station = 'Zadar-aerodrom';
   const url = 'https://vrijeme.hr/hrvatska_n.xml';
   while (true) {
     let temp = null;
